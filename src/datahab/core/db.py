bb4b254168b792ecd9fa974d48da74ecf87b31d9
Anant Bhardwaj <anantb@csail.mit.edu>
2013-10-03 14:16:22 -0400

    test db.py

--- src/datahab/core/db.py
+++ src/datahab/core/db.py
@@ -15,45 +15,76 @@ class Connection:
   def __del__(self):
     self.connection.close()
 
-  def execute_query(self, sql, params=None):
+  def execute_query(self, query, params=None):
     res={'status':False}
-    c = self.connection.cursor()
-    c.execute(sql, params)
-    res['data'] = c.fetchall()
-    c.close()
-    res['status'] = True
+    try:
+      c = self.connection.cursor()
+      c.execute(query, params)
+      res['data'] = c.fetchall()
+      c.close()
+      res['status'] = True
+    except exc:
+      res['error'] = ex
+
     return res
 
   def list_databases(self):
-    s = "SELECT datname FROM pg_catalog.pg_database WHERE NOT datistemplate"
-    return self.execute_query(s)
+    res={'status':False}
+    try:
+      s = "SELECT datname FROM pg_catalog.pg_database WHERE NOT datistemplate"
+      return self.execute_query(s)
+    except exc:
+      res['error'] = exc
+
+    return res
 
   def list_tables(self):
-    s = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
-    return self.execute_query(s);
+    res={'status':False}
+    try:
+      s = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+      return self.execute_query(s);
+    except exc:
+      res['error'] = exc
+
+    return res
 
   def create_database(self, db_name):
     res={'status':False}
-    self.connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
-    c = self.connection.cursor()
-    s = "CREATE DATABASE %s" %(db_name)
-    c.execute(s, None)
-    res={'status':True}
+    try:
+      self.connection.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+      c = self.connection.cursor()
+      s = "CREATE DATABASE %s" %(db_name)
+      c.execute(s, None)
+      res={'status':True}
+    except exc:
+      res['error'] = exc
+
     return res
 
-  def create_table(self, table_name):
+  def create_table(self, query):
     res={'status':False}
-    c = self.connection.cursor()             
-    s = "CREATE TABLE %s (id int)" %(table_name)
-    c.execute(s, None)
-    res={'status':True}
+    try:
+      c = self.connection.cursor()
+      c.execute(query, None)
+      res={'status':True}
+    except exc:
+      res['error'] = exc
+
     return res
 
   def close(self):
-    self.connection.close()
+    res={'status':False}
+    try:
+      self.connection.close()
+      res={'status':True}
+    except exc:
+      res['error'] = exc
+
+    return res
 
 
 if __name__ == '__main__':
   con = Connection()
   print  con.list_databases()
+  con = Connection(db_name='leaderboard')
   print con.list_tables()
