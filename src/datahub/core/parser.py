3a6b9dcc415dfe523485a44a19911d56f2a31450
Anant Bhardwaj <anantb@csail.mit.edu>
2013-10-15 15:01:00 -0400

    use defaultdict

--- src/datahub/core/parser.py
+++ src/datahub/core/parser.py
@@ -3,8 +3,8 @@ import os
 import sys
 import subprocess
 
+from collections import defaultdict
 from collections import OrderedDict
-
 
 '''
 @author: anant bhardwaj
@@ -12,6 +12,7 @@ from collections import OrderedDict
 
 DataHub Model Parser
 '''
+
 
 allowed_type = [
     'boolean',
@@ -57,7 +58,7 @@ sql_map = {
   }
 
 
-def parse(in_file, write_to_file=False):
+def parse(in_file, generate_class=False):
   in_f = open(in_file).read()
   data = json.loads(in_f, object_pairs_hook=OrderedDict)
   thrift_out = ''
@@ -66,36 +67,36 @@ def parse(in_file, write_to_file=False):
 
   for t in data:
     table = t['model']
-    i = 1
-    table_map[table] = {}
+    table_map[table] = defaultdict(dict)
     thrift_out += "struct %s {\n" % (table)
     sql_out += "CREATE TABLE %s (" % (table)
     num_fields = len(t['fields'])
 
+    i = 1
     for field in t['fields']:
       field_type = t['fields'][field]['type']
       if field_type not in allowed_type:
         raise Exception('wrong type: %s' %(t['fields'][field]['type']))
 
-      if field not in table_map[table]:
-        table_map[table][field] = {}
-
       table_map[table][field].update({'type': t['fields'][field]['type']})
 
-      if (field_type == 'foreign_key'):
+      if field_type == 'foreign_key':
         foreign_table = t['fields'][field]['table']
         foreign_field = t['fields'][field]['field']
+
         thrift_out += "  %d: optional %s %s" % (
             i ,
             t['fields'][field]['table'],
             field
           )
+
         sql_out += "%s %s references %s(%s)" % (
             field,
             table_map[foreign_table][foreign_field]['type'],
             foreign_table,
             foreign_field
           )
+
       else:
         thrift_out += "  %d: optional %s %s" % (i , thrift_map[field_type], field)
         sql_out += "%s %s" % (field, sql_map[field_type])
@@ -109,7 +110,7 @@ def parse(in_file, write_to_file=False):
     thrift_out += "\n}\n\n"
     sql_out += ");\n"
   
-  if write_to_file:
+  if generate_class:
     out_f_name = os.path.abspath(os.path.splitext(in_file)[0] + '.thrift')
     out_f = open(out_f_name, 'w+')
     out_f.write(thrift_out)
@@ -124,7 +125,7 @@ def parse(in_file, write_to_file=False):
   
 
 def test():
-  thrift_out, sql_out = parse(sys.argv[1], write_to_file=True)
+  thrift_out, sql_out = parse(sys.argv[1], generate_class=True)
   print thrift_out
   print sql_out
 
