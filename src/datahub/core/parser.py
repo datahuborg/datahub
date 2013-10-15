f0045cd805ad4db37e94b0828eee38a99ea3b108
Anant Bhardwaj <anantb@csail.mit.edu>
2013-10-15 11:09:13 -0400

    make ORM spec more flexible

--- src/datahub/core/parser.py
+++ src/datahub/core/parser.py
@@ -12,31 +12,32 @@ from collections import OrderedDict
 DataHub Model Parser
 '''
 
-def parse(in_file):
+def parse(in_file, write_to_file=False):
   in_f = open(in_file).read()
   data = json.loads(in_f, object_pairs_hook=OrderedDict)
   out = ''
 
-  for t in data.keys():
+  for t in data:
     i = 1
-    out += "struct %s {\n" % (t)
-    for f in data[t].keys():
-      out += "  %d: optional %s %s,\n" % (i , data[t][f]['type'], f)
+    out += "struct %s {\n" % (t['model'])
+    for field in t['fields']:
+      out += "  %d: optional %s %s,\n" % (i , t['fields'][field]['type'], field)
       i += 1
 
     out += "}\n"
     out += "\n"
   
-  out_f = open(os.path.splitext(in_file)[0] + '.thrift', 'w+')
-  out_f.write(out)
-  out_f.close()
+  if write_to_file:
+    out_f = open(os.path.splitext(in_file)[0] + '.thrift', 'w+')
+    out_f.write(out)
+    out_f.close()
 
   return out
 
   
 
 def test():
-  print parse(sys.argv[1])
+  print parse(sys.argv[1], write_to_file=True)
 
 if __name__ == '__main__':
   test()
