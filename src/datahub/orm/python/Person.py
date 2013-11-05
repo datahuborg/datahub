a7352a149496b4565e950a5dae9c69ee7825c13d
Jesika Haria <jesikaharia@gmail.com>
2013-11-05 12:00:43 -0500

    Deleted some unnecessary files.

--- src/datahub/orm/python/Person.py
+++
@@ -1,8 +0,0 @@
-import models
-# will be import datahub.models and super class will be models.BaseModel
-
-class Person(models.BaseModel):
-  first_name = models.CharField(size=30)
-  #last_name = models.CharField(size=30)
-
-
