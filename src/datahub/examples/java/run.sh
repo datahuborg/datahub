b227f3c146403e00530ccc192bb89b26f989ad10
Anant Bhardwaj <anantb@csail.mit.edu>
2013-11-07 15:51:00 -0500

    move jars in third-party

--- src/datahub/examples/java/run.sh
+++ src/datahub/examples/java/run.sh
@@ -4,7 +4,8 @@ rm -rf $abspath/bin
 rm -rf $abspath/gen-java
 thrift --gen java -o $abspath $abspath/../../datahub.thrift
 mkdir $abspath/bin
-CLASSPATH=$CLASSPATH:$abspath/lib/*:$abspath/bin:$abspath/gen-java
+jar=$abspath/../../../third-party/jar
+CLASSPATH=$CLASSPATH:$jar/thrift/libthrift-0.9.1.jar:$jar/slf4j/slf4j-api-1.7.5.jar:$abspath/bin:$abspath/gen-java
 javac -classpath $CLASSPATH -d $abspath/bin $abspath/src/DHClient.java
 export CLASSPATH
 java -classpath $CLASSPATH DHClient
