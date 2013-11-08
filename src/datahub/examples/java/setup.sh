04266e9dc810796340d4d132be3387853f215b49
Anant Bhardwaj <anantb@csail.mit.edu>
2013-11-08 17:00:53 -0500

    required java files

--- src/datahub/examples/java/setup.sh
+++ src/datahub/examples/java/setup.sh
@@ -9,6 +9,6 @@ rm -rf $abspath/gen-java
 thrift --gen java -o $abspath $basepath/src/datahub/datahub.thrift
 mkdir $abspath/bin
 jar=$(cd "$basepath/lib/third-party/java"; pwd)
-CLASSPATH=$CLASSPATH:$jar/thrift/libthrift-0.9.1.jar:$jar/slf4j/slf4j-api-1.7.5.jar:$abspath/bin:$abspath/gen-java
+CLASSPATH=$CLASSPATH:$jar/thrift/*:$jar/slf4j/*:$abspath/bin:$abspath/gen-java
 export CLASSPATH
 javac -classpath $CLASSPATH -d $abspath/bin $abspath/src/DHClient.java
