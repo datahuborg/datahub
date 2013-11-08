48b1f24b35132a9193e3e8ca66ad2f164bec4e03
Anant Bhardwaj <anantb@csail.mit.edu>
2013-11-08 15:41:05 -0500

    node version

--- src/datahub/examples/node/sample_client.js
+++ src/datahub/examples/node/sample_client.js
@@ -5,9 +5,28 @@
 Sample javascript code accesing DataHub APIs
 */
 
-var transport = new Thrift.Transport("/datahub");
-var protocol  = new Thrift.Protocol(transport);
-var client    = new DataHubClient(protocol);
+var thrift = require('thrift'),
+  DataHub = require('./gen-nodejs/DataHub.js'),
+  ttypes = require('./gen-nodejs/datahub_types.js'),
+  connection = thrift.createConnection(
+    'datahub-experimental.csail.mit.edu',
+    9000, {
+    transport: thrift.TBufferedTransport,
+    protocol: thrift.TBinaryProtocol
+  });
 
-var version = client.get_version()
-console.log(version);
+connection.on('connect', function() {
+  var client = thrift.createClient(DataHub, connection);
+  client.get_version(function(err, version) {
+    if (err) {
+      console.log('DataHub error:', err);
+    } else {
+      console.log('DataHub version:', version);
+    }
+    connection.end();
+  });
+});
+
+connection.on('error', function(err){
+  console.log('error', err);
+});
