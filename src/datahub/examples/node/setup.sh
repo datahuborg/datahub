91cdd3c2a22367919d5bfa1f28eda340f4a84d5d
Anant Bhardwaj <anantb@csail.mit.edu>
2013-11-19 16:19:33 -0500

    sample html

--- src/datahub/examples/node/setup.sh
+++ src/datahub/examples/node/setup.sh
@@ -3,7 +3,7 @@ abspath=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
 
 basepath=$(cd "$abspath/../../../.."; pwd)
 
-rm -rf $abspath/gen-js
+rm -rf $abspath/gen-nodejs
 thrift --gen js:node -o $abspath $basepath/src/datahub/datahub.thrift
 
 NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules/:$abspath/gen-nodejs
