b30ad545b06fa1422ad5532f2660b096d94289d7
Anant Bhardwaj <anantb@csail.mit.edu>
2014-03-05 21:32:21 -0500

    some issue with this

--- src/main/examples/node/DHClient.js
+++ src/main/examples/node/DHClient.js
@@ -5,27 +5,22 @@
 Sample nodejs code accesing DataHub APIs
 */
 
-var thrift = require('thrift'),
+var http = require('http')
+  thrift = require('thrift'),
   DataHub = require('DataHub.js'),
-  ttypes = require('datahub_types.js'),
-  connection = thrift.createConnection(
-    'http://datahub.csail.mit.edu/service', {
-    transport: thrift.TBufferedTransport(),
-    protocol: thrift.TBinaryProtocol
-  });
+  ttypes = require('datahub_types.js');
 
-connection.on('connect', function() {
-  var client = thrift.createClient(DataHub, connection);
-  client.get_version(function(err, version) {
-    if (err) {
-      console.log('DataHub error:', err);
-    } else {
-      console.log('DataHub version:', version);
-    }
-    connection.end();
-  });
+var req = http.request("http://datahub.csail.mit.edu/service");
+connection = new thrift.Connection(req);
+var client = thrift.createClient(DataHub, connection);
+client.get_version(function(err, version) {
+  if (err) {
+    console.log('DataHub error:', err);
+  } else {
+    console.log('DataHub version:', version);
+  }
 });
 
-connection.on('error', function(err){
-  console.log('error', err);
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
 });
