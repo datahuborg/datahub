2bc43c01fcb81571bc5355fbb39987e46d1bc2b0
Anant Bhardwaj <anantb@sigmod.csail.mit.edu>
2014-03-06 21:29:06 -0500

    fix the path issue

--- src/main/examples/php/setup.sh
+++ src/main/examples/php/setup.sh
@@ -4,4 +4,4 @@ abspath=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
 basepath=$(cd "$abspath/../../../.."; pwd)
 
 rm -rf $abspath/gen-js
-thrift --gen php -o $abspath $basepath/src/datahub/datahub.thrift
+thrift --gen php -o $abspath $basepath/src/main/datahub.thrift
--- src/main/examples/ruby/setup.sh
+++ src/main/examples/ruby/setup.sh
@@ -3,5 +3,5 @@ abspath=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
 
 basepath=$(cd "$abspath/../../../.."; pwd)
 
-rm -rf $abspath/gen-js
-thrift --gen rb -o $abspath $basepath/src/datahub/datahub.thrift
+rm -rf $abspath/gen-rb
+thrift --gen rb -o $abspath $basepath/src/main/datahub.thrift
