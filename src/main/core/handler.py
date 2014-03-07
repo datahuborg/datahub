d135a5b1414f5716a026968f45cf724a617b9cb9
Anant Bhardwaj <anantb@csail.mit.edu>
2014-03-07 00:58:56 -0500

    paging

--- src/main/core/handler.py
+++ src/main/core/handler.py
@@ -16,7 +16,7 @@ DataHub Handler
 def construct_query_result(res):
   table = DHTable(rows=[
       DHRow(
-          cells=[DHCell(value=bytes(val)) for val in t]
+          cells=[DHCell(value=val) for val in t]
       ) for t in res['tuples']
   ])
 
