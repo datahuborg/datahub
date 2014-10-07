3c0f2be3197d5811360c514be57c547fae6bfc8d
Anant Bhardwaj <anantb@csail.mit.edu>
2014-10-07 17:00:01 -0400

    comments in the sample code

--- src/examples/java/src/SampleClient.java
+++ src/examples/java/src/SampleClient.java
@@ -20,21 +20,25 @@ public class SampleClient {
       TProtocol protocol = new  TBinaryProtocol(transport);
       DataHub.Client client = new DataHub.Client(protocol);
 
-      System.out.println(client.get_version());
+      System.out.println("Version: " + client.get_version());
 
+      // open connection
       ConnectionParams con_params = new ConnectionParams();
       con_params.setUser("anantb");
       con_params.setPassword("anant");
       Connection con = client.open_connection(con_params);
 	     
+      // execute a query
       ResultSet res = client.execute_sql(con, "select * from anantb.test.demo", null);
 
+      // print field names
       for (String field_name : res.getField_names()) {
         System.out.print(field_name + "\t");
       }
 
       System.out.println();
 
+      // print tuples
       for (Tuple t : res.getTuples()) {
         for (ByteBuffer cell : t.getCells()) {
           System.out.print(new String(cell.array()) + "\t");
