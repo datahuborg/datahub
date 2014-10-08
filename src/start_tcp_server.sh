d35a76d4c114bbd9462aec4386f1c13e79870d2e
Anant Bhardwaj <anantb@csail.mit.edu>
2014-10-08 04:22:21 -0400

    minor stuff

--- src/start_tcp_server.sh
+++ src/start_tcp_server.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-source stop_tcp_server.sh
+abspath=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
+
+source $abspath/stop_tcp_server.sh
+
 echo "Starting DataHub TCP server..."
 nohup python $abspath/server.py > /dev/null 2>&1 &
 echo "Done."
