86c2ce10fc1732609b166cc893ca4b4a107e3e5f
Anant Bhardwaj <anantb@csail.mit.edu>
2014-10-12 14:45:52 -0400

    major refactoring

--- src/browser/views.py
+++ src/browser/views.py
@@ -282,7 +282,8 @@ def file_import(request, repo_owner, repo):
     
     manager = get_manager(request, repo_owner)
     manager.execute_sql(query=query)
-    DataHubManager.import_file(path=file_path, table_name=dh_table_name)
+    DataHubManager.import_file(
+        repo_owner=repo_owner, table_name=dh_table_name, file_path=file_path)
     return HttpResponseRedirect('/browse/%s/%s' %(repo_owner, repo))
   except Exception, e:
     return HttpResponse(
@@ -349,7 +350,7 @@ def file_export(request, repo_owner, repo, table_name):
     file_path = '%s/%s.csv' %(repo_dir, table_name)
     dh_table_name = '%s.%s.%s' %(repo_owner, repo, table_name)
     DataHubManager.export_file(
-        repo_owner=repo_owner, path=file_path, table_name=dh_table_name)
+        repo_owner=repo_owner, table_name=dh_table_name, file_path=file_path)
     return HttpResponseRedirect('/browse/%s/%s' %(repo_owner, repo))
   except Exception, e:
     return HttpResponse(
