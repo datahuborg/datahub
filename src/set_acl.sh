543fc5df090f8ef0cbca21c2ba71f0b862942d4a
Anant Bhardwaj <anantb@csail.mit.edu>
2014-10-27 09:58:59 -0400

    add mount script

--- src/set_acl.sh
+++ src/set_acl.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-mkdir /user_data
+# mkfs -t ext4 /dev/disk/by-id/virtio-xxx
+# mkdir /user_data
+# mount /dev/disk/by-id/virtio-xxx /user_data
 chown -R www-data:www-data /user_data/
 chmod -R 3775 /user_data/
 setfacl -d -m g:www-data:rwx /user_data
