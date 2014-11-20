6efd847fd1b4682a87ca0a7832dff014d39fef63
Aaron Elmore <aelmore@csail.mit.edu>
2014-11-20 14:37:38 -0500

    initial create tables

---
+++ src/core/versioning/create_version_tables.sql
@@ -0,0 +1,45 @@
+create table versions (
+ v_id SERIAL primary key,
+ name varchar(100) NOT NULL CHECK (name <> ''),
+ ts date current_timestamp,
+ user_id varchar(100),
+ msg varchar(240) 
+);
+
+create table version_parent (
+  child_id integer references versions(v_id),
+  parent_id integer references versions(v_id)
+);
+
+create table query_log (
+  log_id SERIAL primary key,
+  v_id integer references versions(v_id),
+  ts date current_timestamp,
+  queries json
+);
+
+create table versioned_table (
+  real_name varchar(110) primary key,
+  display_name varchar(100),
+  v_id integer references versions(v_id),
+  copy_on_write boolean default false,
+  ts date current_timestamp
+);
+
+create table versioned_table_parent (
+  child_table varchar(110) references versioned_table(real_name),
+  parent_table varchar(110) references versioned_table(real_name)
+);
+
+create table table_metadata (
+  rid integer,
+  deleted boolean default false,
+  table_rn varchar(110) references versioned_table(real_name)
+);
+
+create table user_head (
+  user_id varchar(100)
+  repo varchar(100)
+  head_version integer references versioned_table(v_id),
+  constraint head primary key(user_id, rep)
+);
