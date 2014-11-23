6f8eea389832d256c6967f7a50a9c4e12fa68b9b
Anant Bhardwaj <anantb@csail.mit.edu>
2014-11-23 15:58:27 -0500

    fix broken link

--- src/apps/refiner/urls.py
+++ src/apps/refiner/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',    
     url(r'^$', 'refiner.views.index'),
+    url(r'^refine-data$', 'refiner.views.refine_data'),
 )
--- src/browser/urls.py
+++ src/browser/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     
     #### Home Page ####
     url(r'^$', 'browser.views.home'),
+    url(r'^about$', include('www.urls')),
     #### End Home ####
 
     #### WWW Pages ####
@@ -153,6 +154,6 @@ urlpatterns = patterns('',
     #### Apps ####
     url(r'^apps/console/', include('console.urls')), # console app
     url(r'^apps/refiner/', include('refiner.urls')), # refiner app
-    url(r'^apps/dbwipes/', include('dbwipes.urls')), # dbwipes app  
+    #url(r'^apps/dbwipes/', include('dbwipes.urls')), # dbwipes app  
     #### End Apps ####
 )
