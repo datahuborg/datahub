ec928cf7dc19d382721d2eae7c52095224e584f7
Harihar Subramanyam <hahasubbu@gmail.com>
2015-03-12 13:19:41 -0400

    Moved datatables stuff into separate directory

---
+++ src/apps/datatables/build.sh
@@ -0,0 +1,5 @@
+mkdir -p ../../browser/static/datatables/
+mkdir -p ../../browser/static/datatables/css
+mkdir -p ../../browser/static/datatables/js
+cp ./client/js/dataTables.extra.js ../../browser/static/datatables/js/dataTables.extra.js
+cp ./client/css/dataTables.bootstrap.extra.css ../../browser/static/datatables/css/dataTables.bootstrap.extra.css
