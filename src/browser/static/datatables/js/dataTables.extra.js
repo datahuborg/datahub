f26b633e78d7b99eb1e4c048e2d75759decf8488
Harihar Subramanyam <hahasubbu@gmail.com>
2015-03-21 14:26:26 -0400

    Prevent nested scrollbars

--- src/browser/static/datatables/js/dataTables.extra.js
+++ src/browser/static/datatables/js/dataTables.extra.js
@@ -5,8 +5,11 @@
     var url_schema = url_base + "schema/" + repo + "/" + table + "/";
     var thisDataTable = this;
 
+    // First get the schema so we can set the column names.
     $.get(url_schema, function(schema_result) {
-      if (schema_result.success) {
+      if (schema_result.success) { // Was the schema retrieved successfully?
+
+        // Create the column definitions to put into the data table.
         var columnDefinitions = [];
         var schema = schema_result.schema;
         for (var i = 0; i < schema.length; i++) {
@@ -16,8 +19,19 @@
           });
         }
 
+        /*
+        // Add buttons to add an "And" or "Or".
+        var addButton = $("<button class='btn btn-primary'>+And</button>");
+        // Add a section to the bottom of each column for computing aggregates.
+        thisDataTable.find('tfoot th').each( function () {
+            var title = thisDataTable.find('thead th').eq( $(this).index() ).text();
+            $(this).html( '<input type="text" placeholder="Filter '+title+'" />\n<button>Here</button>' );
+        } );
+        */
+             
         thisDataTable.DataTable({
           "columnDefs": columnDefinitions,
+          "searching": false,
           "scrollX": true,
           "serverSide": true,
           "ajax": "/apps/datatables/api/table/" + repo + "/" + table + "/"
