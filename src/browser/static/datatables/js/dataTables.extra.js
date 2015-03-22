f752abf82218d4813201722bb8d68d712ad87e12
Harihar Subramanyam <hahasubbu@gmail.com>
2015-03-22 16:43:03 -0400

    Single button type

--- src/browser/static/datatables/js/dataTables.extra.js
+++ src/browser/static/datatables/js/dataTables.extra.js
@@ -22,30 +22,16 @@
         // Add buttons to add filters.
         var buttonHtml ='<div class="input-group">'
         + '<span class="input-group-btn">'
-        + '<button class="btn btn-default dt-bool-button" type="button">AND</button>'
+        + '<button class="btn btn-default dt-op-button" type="button">=</button>'
         + '</span>'
         + '<input type="text" class="form-control" placeholder="Filter...">'
-        + '<span class="input-group-btn">'
-        + '<button class="btn btn-default dt-op-button" type="button">=val</button>'
-        + '</span>'
-        + '</div>';
-
-        var singleEndButtonHtml ='<div class="input-group">'
-        + '<input type="text" class="form-control" placeholder="Filter...">'
-        + '<span class="input-group-btn">'
-        + '<button class="btn btn-default dt-op-button" type="button">=val</button>'
-        + '</span>'
         + '</div>';
 
         thisDataTable.find('tfoot th').each(function(index) {
           var title = thisDataTable.find('thead th').eq( $(this).index() ).text();
           $(this).attr("data-colname", title);
           $(this).html("");
-          if (index > 0) {
-            $(this).append(buttonHtml);
-          } else {
-            $(this).append(singleEndButtonHtml);
-          }
+          $(this).append(buttonHtml);
           $(this).find("input").attr("placeholder", title);
         });
 
@@ -58,12 +44,12 @@
         }); 
 
         var nextOp = {
-          "=val": "!=val",
-          "!=val": "<val",
-          "<val": "<=val",
-          "<=val": ">val",
-          ">val": ">=val",
-          ">=val": "=val"
+          "=": "!=",
+          "!=": "<",
+          "<": "<=",
+          "<=": ">",
+          ">": ">=",
+          ">=": "="
         };
 
         $(document).on("click", ".dt-op-button", function() {
@@ -82,9 +68,8 @@
             }
           },
           "initComplete": function(settings, json) {
-            var andButtonHtml = '<button class="btn btn-primary dtables-plus-and">+And</button>';
             var orButtonHtml = '<button class="btn btn-primary dtables-plus-or">+Or</button>';
-            var colmd = '<div class="col-md-3">' + andButtonHtml + orButtonHtml + '</div>';
+            var colmd = '<div class="col-md-3">' + orButtonHtml + '</div>';
             var html = '<div class="row">' + colmd + '</div>';
             $(".dataTables_info").parent().parent().before(html);
           },
