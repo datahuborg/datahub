98a318a85fdc35bf3205169bbcf0e462d46437c0
Anant Bhardwaj <anantb@csail.mit.edu>
2015-05-19 11:36:16 -0400

    bring dataq back

--- src/examples/python/my_app.py
+++ src/examples/python/my_app.py
@@ -29,22 +29,22 @@ try:
   # execute a query
   res  = client.execute_sql(
       con=con,
-      query='create table if not exists domo.terms (term text)',
+      query='create table if not exists btdemo.teams (names text)',
       query_params=None)
 
   res  = client.execute_sql(
       con=con,
-      query="insert into domo.terms  values('VC Firm')",
+      query="insert into btdemo.teams  values('Telecommunication')",
       query_params=None)
 
   res  = client.execute_sql(
       con=con,
-      query="insert into domo.terms  values('Student Friendly')",
+      query="insert into btdemo.teams  values('Analytics')",
       query_params=None)
 
   res  = client.execute_sql(
       con=con,
-      query='select * from anantb.domo.terms',
+      query='select * from anantb.btdemo.teams',
       query_params=None)
 
   # print field names
