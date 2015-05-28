ee114ca7cc6837a998f3feab3974b6158a5b9631
Albert Carter <albert.r.carter@gmail.com>
2015-05-28 12:55:53 -0400

    update setup.sh text

--- src/setup.sh
+++ src/setup.sh
@@ -7,18 +7,28 @@ thrift --gen py -o $abspath $abspath/thrift/datahub.thrift
 thrift --gen py -o $abspath $abspath/thrift/account.thrift
 
 # generate C++ apis
-( exec $abspath"/examples/cpp/setup.sh" )
+# ( exec $abspath"/examples/cpp/setup.sh" )
 
 # generate Go apis
-( exec $abspath"/examples/go/setup.sh" )
+# ( exec $abspath"/examples/go/setup.sh" )
 
 # generate java apis
-( exec $abspath"/examples/java/setup.sh")
+# ( exec $abspath"/examples/java/setup.sh" )
 
 # generate javascript apis
-# ( exec $abspath"/examples/javascript/setup.sh")
+# These are hosted and don't need to be generated.
 
+# generate objective-c apis
+# ( exec $abspath"/examples/objc/setup.sh" )
 
+# generate python apis
+# ( exec $abspath"/examples/python/setup.sh" )
 
 PYTHONPATH=$abspath:$abspath/gen-py:$abspath/apps
 export PYTHONPATH
+
+echo "\n---Finished setting up some datahub apis.---\n"
+echo "If you need to generate apis for C++, Go, Java, Objective-C, or Python"
+echo "Open this file, uncomment the appropriate lines, and run again:"
+echo "\t$ nano setup.sh"
+echo "\t$ ./setup.sh"
