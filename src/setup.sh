df7b7e2a0ad962bc9d25f8a1a29d946edeb954c8
Albert Carter <albert.r.carter@gmail.com>
2015-05-28 12:23:49 -0400

    generate CPP thrift code on setup

--- src/setup.sh
+++ src/setup.sh
@@ -6,5 +6,12 @@ rm -rf $abspath/gen-py
 thrift --gen py -o $abspath $abspath/thrift/datahub.thrift
 thrift --gen py -o $abspath $abspath/thrift/account.thrift
 
+# generate C++ Code
+( exec $abspath"/examples/cpp/setup.sh" )
+
+# generate Go Code
+# ( exec $abspath"/examples/go/setup.sh" )
+
+
 PYTHONPATH=$abspath:$abspath/gen-py:$abspath/apps
 export PYTHONPATH
