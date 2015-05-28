e566e672f5a2311115375f3a0730e6f2bae910d2
Albert Carter <albert.r.carter@gmail.com>
2015-05-28 12:35:28 -0400

    correct permissions on GO setup.sh. generate go and java code.

--- src/setup.sh
+++ src/setup.sh
@@ -6,11 +6,18 @@ rm -rf $abspath/gen-py
 thrift --gen py -o $abspath $abspath/thrift/datahub.thrift
 thrift --gen py -o $abspath $abspath/thrift/account.thrift
 
-# generate C++ Code
+# generate C++ apis
 ( exec $abspath"/examples/cpp/setup.sh" )
 
-# generate Go Code
-# ( exec $abspath"/examples/go/setup.sh" )
+# generate Go apis
+( exec $abspath"/examples/go/setup.sh" )
+
+# generate java apis
+( exec $abspath"/examples/java/setup.sh")
+
+# generate javascript apis
+# ( exec $abspath"/examples/javascript/setup.sh")
+
 
 
 PYTHONPATH=$abspath:$abspath/gen-py:$abspath/apps
