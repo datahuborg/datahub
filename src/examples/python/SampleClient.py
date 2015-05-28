61f2eec4835283e0e6457a5de33c65f359ba67e8
Albert Carter <albert.r.carter@gmail.com>
2015-05-28 17:09:28 -0400

    update python example

--- src/examples/python/SampleClient.py
+++ src/examples/python/SampleClient.py
@@ -13,8 +13,17 @@ from thrift.transport import TTransport
 
 Sample Python client for DataHub
 '''
+username = "USERNAME"
+password = "PASSWORD"
+repo = "REPO"
+table = "TABLE"
 
 try:
+  # Before running you will need to populate the above variables
+  # and create a corresponding repo and table in your datahub acount
+  print "Trying to connect to datahub with the following credentials..."
+  print "\tu: " +username + "\tp: " + password
+  print "\trepo: " + repo + "\ttable:" + table
   transport = THttpClient.THttpClient('http://datahub.csail.mit.edu/service')
   transport = TTransport.TBufferedTransport(transport)
   protocol = TBinaryProtocol.TBinaryProtocol(transport)
@@ -23,13 +32,17 @@ try:
   print "Version: %s" %(client.get_version())
   
   # open connection
-  con_params = ConnectionParams(user='anantb', password='anant')
+  con_params = ConnectionParams(user=username, password=password)
   con = client.open_connection(con_params=con_params)
 
+
   # execute a query
+  query = "select * from " + username + "." + repo + "." + table
+  print "\nTrying to execute the followin query:\t" + query + "..."
+
   res  = client.execute_sql(
       con=con,
-      query='select * from anantb.test.demo',
+      query=query,
       query_params=None)
 
   # print field names
