dd285d41f6d8f0d855f7b2b3ef3b2fb3ada88664
Denis Babani <dns.server@gmail.com>
2015-06-02 17:06:08 -0400

    adding Dockerfile

--- provisions/setup.sh
+++ provisions/setup.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
-apt-get -y install postgresql \
-                   postgresql-contrib \
-                   postgresql-server-dev-all \
-                   python-dev python-pip \
-                   thrift-compiler
-sudo -u postgres createdb datahub
-sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'postgres';"
+apt-get update
+
+apt-get -y install wget
+wget -qO- https://get.docker.com/ | sh
 
 
-mkdir /user_data
-
-cd /vagrant
-pip install virtualenv
-
-virtualenv venv
-pip install -r requirements.txt
-
-source src/setup.sh
-python src/manage.py syncdb
-python src/manage.py migrate inventory
+# in the container
+# apt-get -y install postgresql \
+#                    postgresql-contrib \
+#                    postgresql-server-dev-all \
+#                    python-dev python-pip \
+#                    thrift-compiler
+#
+# service postgresql start
+#
+# sudo -u postgres createdb datahub
+# sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'postgres';"
+#
+#
+# mkdir /user_data
+#
+# cd /vagrant
+# pip install virtualenv
+#
+# virtualenv venv
+# pip install -r requirements.txt
+#
+# source src/setup.sh
+# python src/manage.py syncdb
+# python src/manage.py migrate inventory
+#
+#
+# wget -qO- https://get.docker.com/ | sh
+#
+# b9b0daa57b2c
