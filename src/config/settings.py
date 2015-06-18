f73f8428b1c8fd600c5badc84b323540c9c3f303
Anant Bhardwaj <anantb@csail.mit.edu>
2015-06-18 10:49:42 -0700

    set defaults correctly and put a blank line to remove the clutter in the table view

--- src/config/settings.py
+++ src/config/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
     'NAME': 'datahub',                      # Or path to database file if using sqlite3.
     'USER': 'postgres',                      # Not used with sqlite3.
     'PASSWORD': 'postgres',                  # Not used with sqlite3.
-    'HOST': 'db',                      # Set to empty string for localhost. Not used with sqlite3.
+    'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
     'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
   }
 }
