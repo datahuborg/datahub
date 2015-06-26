8f693567519025617b25630abf3f3122506d7509
Albert Carter <albert.r.carter@gmail.com>
2015-06-26 16:25:01 -0400

    remove old developer apis page. now using sphinx

--- src/www/urls.py
+++ src/www/urls.py
@@ -2,6 +2,4 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',    
     url(r'^$', 'www.views.index'),
-    url(r'^developer$', 'www.views.developer_apis'),
-    url(r'^developer/$', 'www.views.developer_apis'),
 )
