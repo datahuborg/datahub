2285544263344f6a4bebb969d9bf8946db07fe9f
Albert Carter <albert.r.carter@gmail.com>
2015-06-29 16:54:45 -0400

    update auth page tests

--- src/account/test/test_views.py
+++ src/account/test/test_views.py
@@ -1,55 +1,108 @@
+import hashlib
+
 from django.test import TestCase
-
 from django.test import Client
+from django.http import HttpRequest
+from django.core.urlresolvers import resolve
 
-class SiteAccountPages(TestCase):
+from inventory.models import User
+from account.auth import login, register, clear_session, logout, forgot, verify, reset, jdbc_password 
+
+
+class LoginPageTest(TestCase):
+
     def setUp(self):
         self.client = Client(enforce_csrf_checks=False)
-        
+        self.user, created = User.objects.get_or_create(id=10, username="user", 
+            password="pass", email="noreply.csail.mit.edu", f_name="f_name", l_name="l_name", 
+            active=True)
 
-    def test_login(self):
-        # self.response = self.client.get(reverse('/'))
-        # self.assertEqual(self.response.status_code, 200)
-        # SimpleTestCase.assertContains(response, text, count=None, status_code=200, msg_prefix='', html=False)
+    def test_login_url_resolves_to_login_page_view(self):
+        found = resolve('/account/login')
+        self.assertEqual(found.func, login)
+
+    def test_login_page_returns_correct_template(self):
         response = self.client.get('/account/login', follow=True)
-
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'login.html')
 
-    def test_register(self):
-        response = self.client.get('/account/register', follow=True)
+    def test_login_page_allows_user_to_login(self):
+        login_credentials = {'login_id': self.user.username, 
+        'login_password': self.user.password}
+        response = self.client.post('/account/login', login_credentials) 
+        
+        self.assertEqual(response.status_code, 200)
+        self.fail('test not completed. Users the test creates cannot \
+            authenticate because their password hashes will not match.')
 
+
+class RegisterPageTest(TestCase):
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=False)
+        
+    def test_register_url_resolves_to_register_page_view(self):
+        found = resolve('/account/register')
+        self.assertEqual(found.func, register)
+
+    def test_register_page_returns_correct_template(self):
+        response = self.client.get('/account/register', follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'register.html')
 
-    def test_logout(self):
-        # test that the page exists, not that it actually works
-        response = self.client.get('/account/logout')
+    def test_register_page_allows_new_user_registration(self):
+        self.fail('test not yet implemented')
 
+    def test_registered_user_actually_exists(self):
+        self.fail('test not yet implemented')
+
+
+class LogoutPageTest(TestCase):
+    def setUp(self):
+         self.client = Client(enforce_csrf_checks=False)
+         # create a new user
+
+    def test_logout_url_resolves_to_logout_page_view(self):
+        found = resolve('/account/logout')
+        self.assertEqual(found.func, logout)
+
+    def test_logout_page_returns_correct_template(self):
+        response = self.client.get('/account/logout')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'confirmation.html')
 
-    def test_forgot_get(self):
+    def test_logout_page_actually_logs_users_out(self):
+        self.fail('test not yet implemented')
+
+
+class ForgotPasswordPageTest(TestCase):
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=False)
+        # create a new user
+
+    def test_forgot_password_page_resolves_to_forgot_page_view(self):
+        found = resolve('/account/forgot')
+        self.assertEqual(found.func, forgot)
+
+    def test_forgot_password_page_returns_correct_template(self):
         response = self.client.get('/account/forgot', follow=True)
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'forgot.html')
 
-    def test_forgot_post_bad_email(self):
-        # tests that reset page can be posted to with wrong email addresses
-        # response = self.client.post('/account/forgot', {'email', 'noemailhere@noreply.noo'})
-        # self.assertEqual(response.status_code, 200)
-        # returns a 403 due
-
-        # self.assertTemplateUsed(response, 'forgot.html')
-        pass
-
-    def test_forgot_post_good_email(self):
+    def test_forgot_post_with_valid_email(self):
         # test is incomplete. requires a user in the database
         # response = self.client.post('/account/forgot', {'email', 'goodemailhere@mydomain.yes'})
         # self.assertEqual(response.status_code, 200)
-        pass
+        self.fail('test not yet implemented')
 
+    def test_forgot_post_bad_email(self):
+        self.fail('test not yet implemented')
+
+
+class JdbcPasswordTest(TestCase):
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=False)
+        # create a new user
 
     def test_jdbc_password_unauthenticated(self):
         # test that this redirects unauthentiated users to the login page
@@ -62,20 +115,17 @@ class SiteAccountPages(TestCase):
         # hashing is correct.
         response = self.client.get('/account/jdbc_password', follow=True)
         self.assertEqual(response.status_code, 200)
+        self.fail('test not yet implemented')
+
+class ResetPasswordTest(TestCase):
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=False)
+        # create a new user
 
     def test_reset_password(self):
         # response = self.client.post('/account/reset', 
         #     {'user_email': '', 'new_password': 'smith'})
         # self.assertEqual(response.status_code, 200)
         # self.assertTemplateUsed(response, 'login.html')
-        pass
-
-
-    def test_verify(self):
-        pass
-
-
-
-
-
+        self.fail('test not yet implemented')
 
