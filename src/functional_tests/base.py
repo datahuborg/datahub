c2bb86876bc127f3062450de93025bdd4862c059
Albert Carter <albert.r.carter@gmail.com>
2015-10-29 16:58:31 -0400

    allow test_external_links function to be called elsewhere

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -1,4 +1,5 @@
 import sys
+import requests
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 
@@ -28,3 +29,28 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
+
+    def test_external_links(self):
+        # Justin gets a list of external links
+        links = self.browser.find_elements_by_xpath(
+            "//a[(starts-with(@href, 'http'))]")
+        links = map(lambda x: x.get_attribute('href'), links)
+
+        # he prepares to make note of which tests fail
+        failing_links = []
+
+        # He tries each of them, to make sure that they work
+        for link in links:
+            try:
+                r = requests.get(link, verify=False)
+                if r.status_code != (200 or 302):
+                    failing_link = {'link': link, 'reason': r.status_code}
+                    failing_links.append(failing_link)
+            except:
+                failing_link = {'link': link, 'reason': 'exception'}
+                failing_links.append(failing_link)
+
+        # If there are links that failed, the test fails
+        if len(failing_links) > 0:
+            print("Some links on the did not check out")
+            self.fail(failing_links)
--- src/functional_tests/test_layout_and_styling.py
+++ src/functional_tests/test_layout_and_styling.py
@@ -1,5 +1,4 @@
 from .base import FunctionalTest
-import requests
 import re
 
 
@@ -20,30 +19,9 @@ class LayoutAndStylingTest(FunctionalTest):
 
     def test_front_page_links(self):
         self.browser.get(self.server_url)
-        self.browser.set_window_size(1024, 768)
+        self.browser.set_window_size(1024, 768) 
 
-        # Justin gets a list of external links
-        links = self.browser.find_elements_by_xpath(
-            "//a[(starts-with(@href, 'http'))]")
-        links = map(lambda x: x.get_attribute('href'), links)
-
-        # he prepares to make note of which tests fail
-        failing_links = []
-
-        # He tries each of them, to make sure that they work
-        for link in links:
-            try:
-                r = requests.get(link, verify=False)
-                if r.status_code != (200 or 302):
-                    failing_link = {'link': link, 'reason': r.status_code}
-                    failing_links.append(failing_link)
-            except:
-                failing_link = {'link': link, 'reason': 'exception'}
-                failing_links.append(failing_link)
-
-        # If there are links that failed, the test fails
-        if len(failing_links) > 0:
-            print("Some links on the front page did not check out")
-            self.fail(failing_links)
+        # he verifies that all external links on the home page work
+        self.test_external_links()
 
         # Satisfied, he takes a nap
