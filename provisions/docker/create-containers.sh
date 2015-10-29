352a68db05b1ecb88935d7c0a87cb96b94adac6c
Justin Anderson <jander@mit.edu>
2015-10-29 16:53:38 -0400

    Reorder Docker --volumes-from commands. DAT-1 DAT-24 DAT-36
    All of the user data and db files were being stored in `logs` because `logs` came after `data` when the containers were created.

--- provisions/docker/create-containers.sh
+++ provisions/docker/create-containers.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Put your own certificate and private key at /ssl/nginx.crt and /ssl/nginx.key
+# respectively on the host to have them picked up by the nginx container.
+
+# The order of `--volumes-from` is important. The last one wins.
+#
+# `logs`, `data`, `db` are all based on the same image, so they have
+# conflicting `VOLUME` declarations.
+# - The standard postgres image declares the volume `/var/lib/postgresql/data`.
+# - datahuborg/postgres inherits that and adds the volume `/user_data`.
+# - The `logs` container below adds volumes for three log directories.
+#
+# When `db` includes volumes from `logs`, all five of those paths are overlaid
+# in place of `db`'s content. Adding `data` last then overlays its content for
+# the original two paths.
+#
+# This is necessary because only the postgres image has a postgres user with a
+# logs directory that the user can write to, and the volume to the postgres
+# data path is declared in the base postgres image, so having one requires the
+# other.
+
 echo "Creating Docker containers..."
 echo "(1/5) Creating \"logs\" - Data container for all server logs"
 docker create --name logs \
@@ -14,19 +34,19 @@ docker create --name data \
     datahuborg/postgres
 echo "(3/5) Creating \"db\" - Postgres server"
 docker create --name db \
-    --volumes-from data \
     --volumes-from logs \
+    --volumes-from data \
     datahuborg/postgres
 echo "(4/5) Creating \"app\" - gunicorn server hosting DataHub"
 docker create --name app \
-    --volumes-from data \
     --volumes-from logs \
+    --volumes-from data \
     --link db:db \
     datahuborg/datahub gunicorn --config=provisions/gunicorn/config.py browser.wsgi
 echo "(5/5) Creating \"web\" - nginx http proxy"
 docker create --name web \
-    --volumes-from app \
     --volumes-from logs \
+    --volumes-from app \
     -v /ssl/:/etc/nginx/ssl/ \
     --link app:app \
     -p 80:80 -p 443:443 \
