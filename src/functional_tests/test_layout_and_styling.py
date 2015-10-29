8c3cc8cd733c423e2a63eac23cdb149d704d0ed3
Albert Carter <albert.r.carter@gmail.com>
2015-10-29 18:04:30 -0400

    test login and sign in pages.

--- src/functional_tests/test_layout_and_styling.py
+++ src/functional_tests/test_layout_and_styling.py
@@ -2,7 +2,7 @@ from .base import FunctionalTest
 import re
 
 
-class LayoutAndStylingTest(FunctionalTest):
+class LayoutAndStylingUnauthenticated(FunctionalTest):
 
     def test_front_page_content(self):
         # Justin goes to the home page
@@ -18,10 +18,46 @@ class LayoutAndStylingTest(FunctionalTest):
         self.assertNotEqual(text_found, None)
 
     def test_front_page_links(self):
+        # Justin goes to the home page
         self.browser.get(self.server_url)
-        self.browser.set_window_size(1024, 768) 
+        self.browser.set_window_size(1024, 768)
 
         # he verifies that all external links on the home page work
         self.test_external_links()
 
-        # Satisfied, he takes a nap
+    def test_login_signup_pages_content(self):
+        # Justin goes to the home page
+        self.browser.get(self.server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Justin clicks the "Sign In" button
+        self.browser.find_element_by_id('id_sign_in').click()
+        login_url = self.browser.current_url
+
+        # The word "email" appears in the page
+        src = self.browser.page_source
+        text_found = re.search(r'email', src)
+        self.assertNotEqual(text_found, None)
+
+        # the word "password" appears in the page
+        src = self.browser.page_source
+        text_found = re.search(r'password', src)
+        self.assertNotEqual(text_found, None)
+
+        # Justin realizes that he needs to sign up, and clicks "Sign Up"
+        self.browser.find_element_by_id('id_sign_up').click()
+        signup_url = self.browser.current_url
+
+        # Login and Signup are on different pages
+        self.assertNotEqual(signup_url, login_url)
+
+        # the word "email" appears in the page
+        src = self.browser.page_source
+        text_found = re.search(r'password', src)
+        self.assertNotEqual(text_found, None)
+
+
+
+# class LayoutAndStylingLoginPageTest(FunctionalTest):
+
+
