b3b61527967d3cf892298502b113d82bad358625
Justin Anderson <jander@mit.edu>
2015-11-04 15:08:41 -0500

    Remove reference to registration library.

--- src/account/urls.py
+++ src/account/urls.py
@@ -2,20 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.dispatch import receiver
-from registration.signals import user_registered
-
-@receiver(user_registered)
-def registration_callback(sender, **kwargs):
-    # Create the user's repo and give them a Postgres user here
 
 urlpatterns = patterns(
     '',
-    # url(r'^', include('registration.backends.simple.urls')),
-    # url('^register', CreateView.as_view(
-    #         template_name='registration/register.html',
-    #         form_class=UserCreationForm,
-    #         success_url='/'
-    # )),
     # url(r'^', include('django.contrib.auth.urls')),
     # url(r'^old/login', 'account.auth.login'),
     # url(r'^old/register', 'account.auth.register'),
