7fa9769078538e22c14b36f4096a58f692070104
Albert Carter <albert.r.carter@gmail.com>
2015-11-09 17:36:29 -0500

    pep8-ify

--- src/browser/test/test_database_and_repo_subviews.py
+++ src/browser/test/test_database_and_repo_subviews.py
@@ -4,7 +4,6 @@ from django.test import Client
 from django.core.urlresolvers import resolve
 from django.contrib.auth.models import User
 from core.db.manager import DataHubManager
-from browser.views import repo_create, repo_delete
 import browser.views
 from mock import MagicMock, patch
 
@@ -19,6 +18,7 @@ from mock import MagicMock, patch
 # into the database. There's not a good way around this for now.
 
 class CreateAndDeleteRepo(TestCase):
+
     def setUp(self):
         self.client = Client(enforce_csrf_checks=False)
 
@@ -26,12 +26,14 @@ class CreateAndDeleteRepo(TestCase):
         self.username = "test_username"
         self.password = "test_password"
         self.email = "test_email@csail.mit.edu"
-        self.user = User.objects.create_user(self.username, self.email, self.password)
+        self.user = User.objects.create_user(
+            self.username, self.email, self.password)
         self.hashed_password = hashlib.sha1(self.password).hexdigest()
 
         # create user's database
-        DataHubManager.create_user(username=self.username, password=self.hashed_password)
-        
+        DataHubManager.create_user(
+            username=self.username, password=self.hashed_password)
+
         # log the user in
         self.client.login(username=self.username, password=self.password)
 
@@ -43,23 +45,24 @@ class CreateAndDeleteRepo(TestCase):
 
     def test_create_repo_resolves_to_correct_view_function(self):
         try:
-            found = resolve('/create/' + self.username+ '/repo/')
+            found = resolve('/create/' + self.username + '/repo/')
         except:
-            self.fail("exception at test_create_repo_resolves_to_create_func hit")
+            self.fail(
+                "exception at test_create_repo_resolves_to_create_func hit")
 
-        self.assertEqual(found.func, repo_create)
+        self.assertEqual(found.func, browser.views.repo_create)
 
     def test_create_repo_returns_correct_page(self):
         try:
-            login_credentials = {'login_id': self.username, 
-            'login_password': self.password}
+            login_credentials = {'login_id': self.username,
+                                 'login_password': self.password}
 
-            response = self.client.get('/create/' + self.username + '/repo', follow=True)
+            response = self.client.get(
+                '/create/' + self.username + '/repo', follow=True)
         except:
             self.fail("exception at test_create_repo_returns_correct_page hit")
 
         self.assertTemplateUsed(response, 'repo-create.html')
-
 
     @patch('core.db.manager.DataHubManager.create_repo')
     def test_create_repo_calls_correct_function(self, mock_create_repo):
@@ -70,7 +73,7 @@ class CreateAndDeleteRepo(TestCase):
             mock_create_repo.return_value = None
             post_object = {'repo': 'repo_name'}
             self.client.post('/create/' + self.username + '/repo', post_object)
-            
+
         except:
             self.fail("exception at test_create_repo_creates_a_repo")
         mock_create_repo.assert_called_once_with('repo_name')
@@ -81,8 +84,9 @@ class CreateAndDeleteRepo(TestCase):
             # create the new repo
             mock_create_repo.return_value = None
             post_object = {'repo': 'repo_name'}
-            self.client.post('/create/' + 'bac_username' + '/repo', post_object)
-            
+            self.client.post(
+                '/create/' + 'bac_username' + '/repo', post_object)
+
         except:
             self.fail("exception at test_create_repo_creates_a_repo")
 
@@ -94,10 +98,10 @@ class CreateAndDeleteRepo(TestCase):
         try:
             found = resolve('/delete/' + self.username + '/repo/')
         except:
-            self.fail("exception at test_delete_repo_resolves_to_correct_view_function")
+            self.fail(
+                "exception at test_delete_repo_resolves_to_correct_view_function")
 
-        self.assertEqual(found.func, repo_delete)
-
+        self.assertEqual(found.func, browser.views.repo_delete)
 
     @patch('core.db.manager.DataHubManager.delete_repo')
     def test_delete_repo_calls_correct_function(self, mock_delete_repo):
@@ -109,7 +113,6 @@ class CreateAndDeleteRepo(TestCase):
             self.fail('exception at test_delete_repo_calls_correct_function')
 
         self.assertEqual(mock_delete_repo.call_count, 1)
-
 
     @patch('core.db.manager.DataHubManager.delete_repo')
     def test_delete_cannot_happen_on_another_user_acct(self, mock_delete_repo):
@@ -123,9 +126,8 @@ class CreateAndDeleteRepo(TestCase):
         mock_delete_repo.assert_not_called()
 
 
-
-
 # class RepoFeaturePages(TestCase):
+
 #     def setUp(self):
 #         self.client = Client(enforce_csrf_checks=False)
 
@@ -133,11 +135,13 @@ class CreateAndDeleteRepo(TestCase):
 #         self.username = "test_username"
 #         self.password = "test_password"
 #         self.email = "test_email@csail.mit.edu"
-#         self.user = User.objects.create_user(self.username, self.email, self.password)
+#         self.user = User.objects.create_user(
+#             self.username, self.email, self.password)
 #         self.hashed_password = hashlib.sha1(self.password).hexdigest()
 
 #         # create user's database
-#         DataHubManager.create_user(username=self.username, password=self.hashed_password)
+#         DataHubManager.create_user(
+#             username=self.username, password=self.hashed_password)
 
 #         # create a repo
 #         self.repo_base = 'test_repo'
@@ -150,31 +154,30 @@ class CreateAndDeleteRepo(TestCase):
 #         DataHubManager.remove_user_and_database(username=self.username)
 #         # remove the postgres db. User will log out automatically.
 
-
 #     def test_table_view_returns_correct_function(self):
-#         try:
-#             found = resolve('/browse/' + self.username + "/" + self.repo_base + "/tables")
-#         except:
-#             self.fail("exception at test_create_repo_resolves_to_create_func hit")
-
-#         self.assertEqual(found.func, browser.views.table)
-
-
-#     def test_create_table(self):
 #         pass
+#         self.assertEqual(1, 1)
+        # try:
+        #     found = resolve('/browse/' + self.username + "/" + self.repo_base + "/tables")
+        # except:
+        #     self.fail("exception at test_create_repo_resolves_to_create_func hit")
 
-#     def createCard(self):
+        # self.assertEqual(found.func, browser.views.table)
 
-#         pass
+    # def test_create_table(self):
+    #     pass
 
-#     def createAnotation(self):
-#         pass
+    # def createCard(self):
+    #     pass
 
-#     def deleteRepo(self):
-#         pass
+    # def createAnotation(self):
+    #     pass
 
-#     def deleteCard(self):
-#         pass
+    # def deleteRepo(self):
+    #     pass
 
-#     def deleteAnnotation(self):
-#         pass
+    # def deleteCard(self):
+    #     pass
+
+    # def deleteAnnotation(self):
+    #     pass
