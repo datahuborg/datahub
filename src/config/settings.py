d69b178dba03a4056c81546ed0d901b683d13959
Justin Anderson <jander@mit.edu>
2015-11-16 12:25:18 -0500

    Make settings.py peppy.

--- src/config/settings.py
+++ src/config/settings.py
@@ -7,21 +7,22 @@ TEMPLATE_DEBUG = DEBUG
 SETTINGS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 ADMINS = (
-  ('Anant Bhardwaj', 'anantb@csail.mit.edu'),
+    ('Anant Bhardwaj', 'anantb@csail.mit.edu'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-    'NAME': 'datahub',                      # Or path to database file if using sqlite3.
-    'USER': 'postgres',                      # Not used with sqlite3.
-    'PASSWORD': 'postgres',                  # Not used with sqlite3.
-    'HOST': 'db',                        # Change to localhost if not using the Vagrant/Docker setup.
-                                         # Docker adds the db container to /etc/hosts automatically.
-    'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'datahub',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        # Change HOST to localhost if not using the Vagrant/Docker setup.
+        # Docker adds the db container to /etc/hosts automatically.
+        'PORT': '5432',
+    }
 }
 
 
@@ -64,17 +65,17 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-  # Put strings here, like "/home/html/static" or "C:/www/django/static".
-  # Always use forward slashes, even on Windows.
-  # Don't forget to use absolute paths, not relative paths.
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
 )
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
-  'django.contrib.staticfiles.finders.FileSystemFinder',
-  'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Generate a new SECRET_KEY with `python src/manage.py generatesecretkey`.
@@ -85,38 +86,38 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'APP_DIRS': True,
         'OPTIONS': {
-          'context_processors': (
-            'django.contrib.auth.context_processors.auth',
-            'django.template.context_processors.debug',
-            'django.template.context_processors.i18n',
-            'django.template.context_processors.media',
-            'django.template.context_processors.static',
-            'django.template.context_processors.tz',
-            'django.contrib.messages.context_processors.messages',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
-            'social.apps.django_app.context_processors.backends',
-            'social.apps.django_app.context_processors.login_redirect',
-          )
+            'context_processors': (
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'social.apps.django_app.context_processors.backends',
+                'social.apps.django_app.context_processors.login_redirect',
+            )
         }
     }
 ]
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-  'django.template.loaders.filesystem.Loader',
-  'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    # 'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
-  'django.middleware.common.CommonMiddleware',
-  'django.contrib.sessions.middleware.SessionMiddleware',
-  'django.middleware.csrf.CsrfViewMiddleware',
-  'django.contrib.auth.middleware.AuthenticationMiddleware',
-  'django.contrib.messages.middleware.MessageMiddleware',
-  # Uncomment the next line for simple clickjacking protection:
-  # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'browser.urls'
@@ -125,32 +126,33 @@ ROOT_URLCONF = 'browser.urls'
 WSGI_APPLICATION = 'browser.wsgi.application'
 
 TEMPLATE_DIRS = (
-  # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-  # Always use forward slashes, even on Windows.
-  # Don't forget to use absolute paths, not relative paths.
+    # Put strings here, like "/home/html/django_templates" or
+    # "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
 )
 
 INSTALLED_APPS = (
-  'django.contrib.auth',
-  'django.contrib.contenttypes',
-  'django.contrib.sessions',
-  'django.contrib.sites',
-  'django.contrib.messages',
-  'django.contrib.staticfiles',
-  'crispy_forms',
-  'social.apps.django_app.default',
-  'account',
-  'console',
-  'browser',
-  'oidc',
-  'dataq',
-  'sentiment',
-  'datatables',
-  'dbwipes',
-  'inventory',
-  'refiner',
-  'viz2',
-  'www'
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'crispy_forms',
+    'social.apps.django_app.default',
+    'account',
+    'console',
+    'browser',
+    'oidc',
+    'dataq',
+    'sentiment',
+    'datatables',
+    'dbwipes',
+    'inventory',
+    'refiner',
+    'viz2',
+    'www'
 )
 
 # django.contrib.auth settings
@@ -162,7 +164,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
-SOCIAL_AUTH_REDIRECT_IS_HTTPS = True  # Make sure OAuth redirects use HTTPS, e.g. https://localhost/complete/twitter
+# Make sure OAuth redirects use HTTPS, e.g. https://localhost/complete/twitter
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
 
 SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.social_auth.social_details',
@@ -178,8 +181,8 @@ SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.user.user_details'
 )
 
-SOCIAL_AUTH_FIELDS_STORED_IN_SESSION = ['preferred_username',]
-FIELDS_STORED_IN_SESSION = ['preferred_username',]
+SOCIAL_AUTH_FIELDS_STORED_IN_SESSION = ['preferred_username']
+FIELDS_STORED_IN_SESSION = ['preferred_username']
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
@@ -187,27 +190,27 @@ FIELDS_STORED_IN_SESSION = ['preferred_username',]
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
-  'version': 1,
-  'disable_existing_loggers': False,
-  'filters': {
-    'require_debug_false': {
-      '()': 'django.utils.log.RequireDebugFalse'
-    }
-  },
-  'handlers': {
-    'mail_admins': {
-      'level': 'ERROR',
-      'filters': ['require_debug_false'],
-      'class': 'django.utils.log.AdminEmailHandler'
-    }
-  },
-  'loggers': {
-    'django.request': {
-      'handlers': ['mail_admins'],
-      'level': 'ERROR',
-      'propagate': True,
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
     },
-  }
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
 }
 
 # Instances should create a local_settings.py to define custom settings.
