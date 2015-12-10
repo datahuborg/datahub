0d5e6378ea016a3fcaca13dbd76aafc46833286e
Justin Anderson <jander@mit.edu>
2015-12-10 17:36:24 -0500

    Make separate script to back up a single user db.
    Make scripts show their actions to users a little better.

---
+++ provisions/docker/back-up-all-databases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE="/tmp/backup.gz"
+echo "Backing up all databases from db container into $FILE on host machine..."
+if [ ! -f $FILE ]; then
+    (set -x;
+    docker run --rm -t --link db:db -v /tmp:/tmp datahuborg/postgres \
+        /bin/bash -c \
+        "pg_dumpall --clean --if-exists --host db --username postgres | gzip > $FILE")
+else
+    echo "$FILE already exists. Cancelling backup."
+fi
+echo "Done."
---
+++ provisions/docker/back-up-single-database.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE="/tmp/$1-backup.gz"
+echo "Backing up database $1 from db container into $FILE on host machine..."
+if [ ! -f $FILE ]; then
+    (set -x;
+    docker run --rm -t --link db:db -v /tmp:/tmp datahuborg/postgres \
+        /bin/bash -c \
+        "pg_dump --clean --create --if-exists --host db --username postgres -d $1 | gzip > $FILE")
+else
+    echo "$FILE already exists. Cancelling backup."
+fi
+echo "Done."
