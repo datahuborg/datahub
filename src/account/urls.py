5217710037dc294b041a37b653379fd3c704ae7f
Justin Anderson <jander@mit.edu>
2015-12-22 15:32:40 -0500

    Add remaining templates for password resetting.

--- src/account/urls.py
+++ src/account/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns(
     # Use Django's built-in password reset tools.
     url(r'^forgot/?$', 'django.contrib.auth.views.password_reset',
         {
-            'template_name': 'forgot.html',
+            'template_name': 'password_reset.html',
             'email_template_name': 'password_reset_email.html',
             'subject_template_name': 'password_reset_subject.txt',
             'password_reset_form': account_forms.ForgotPasswordForm,
@@ -21,15 +21,21 @@ urlpatterns = patterns(
     url(r'^forgot/done/?$',
         'django.contrib.auth.views.password_reset_done',
         {
-            'template_name': 'forgot-done.html',
+            'template_name': 'password_reset_done.html',
         },
         name='password_reset_done'),
     url((r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/'
          r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/?$'),
         'django.contrib.auth.views.password_reset_confirm',
+        {
+            'template_name': 'password_reset_confirm.html',
+        },
         name='password_reset_confirm'),
     url(r'^reset/done/?$',
         'django.contrib.auth.views.password_reset_complete',
+        {
+            'template_name': 'password_reset_complete.html',
+        },
         name='password_reset_complete'),
     # url(r'^verify/$', 'account.views.verify_email'),
 )
