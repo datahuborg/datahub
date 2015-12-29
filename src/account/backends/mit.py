a1325208790a150a3bc184e3983e37c8dade0a3c
Justin Anderson <jander@mit.edu>
2015-12-29 16:54:35 -0500

    Refactor MIT OIDC support and improve security.
    - Support all algorithms for signed userinfo responses.
    - Verify JWS using MIT's JWK set.
    - Make sure claims are validated.
    - Protect against signing algorithm substitution attacks.
    - Refactor general OIDC code out from MIT specific bits.

--- src/account/backends/mit.py
+++ src/account/backends/mit.py
@@ -1,64 +1,14 @@
-from social.backends.open_id import OpenIdConnectAuth, OpenIdConnectAssociation
-from jwt import InvalidTokenError, decode as jwt_decode
-from social.exceptions import AuthTokenError
+from account.backends.oidc import DataHubFixedOpenIdConnect
 
-import six
-import time
 
-from jwkest import JWKESTException
-from jwkest.jwk import KEYS
-from jwkest.jws import JWS
-
-# Most of this comes from https://github.com/omab/python-social-auth/pull/747,
-# an unmerged pull request on Python Social Auth.
-#
-# PSA 0.2.13's OIDC support is very specific to Google's OIDC server. It only
-# supports HS256 signed tokens, doesn't handle signed userinfo responses, and
-# doesn't handle situations where the OIDC server's clock is ahead of the
-# client's.
-
-class _cache(object):
+class MITOpenIdConnect(DataHubFixedOpenIdConnect):
 
     """
-    Caches method return values for a specific length of time.
+    PSA backend for MIT's OpenID Connect server at oidc.mit.edu.
 
-    It maintains a cache per class, so subclasses have a different cache entry
-    for the same cached method.
-
-    Does not work for methods with arguments.
+    Allows anyone with an @mit.edu email address to log in.
     """
 
-    def __init__(self, ttl):
-        self.ttl = ttl
-        self.cache = {}
-
-    def __call__(self, fn):
-        def wrapped(this):
-            now = time.time()
-            last_updated = None
-            cached_value = None
-            if this.__class__ in self.cache:
-                last_updated, cached_value = self.cache[this.__class__]
-            if not cached_value or now - last_updated > self.ttl:
-                try:
-                    cached_value = fn(this)
-                    self.cache[this.__class__] = (now, cached_value)
-                except:
-                    # Use previously cached value when call fails, if available
-                    if not cached_value:
-                        raise
-            return cached_value
-        return wrapped
-
-
-def _autoconf(name):
-    """fget helper function to fetch values from the OIDC config."""
-    def getter(self):
-        return self.oidc_config().get(name)
-    return getter
-
-
-class MITOpenIdConnect(OpenIdConnectAuth):
     name = 'mit-oidc'
 
     OIDC_URL = 'https://oidc.mit.edu'
@@ -66,123 +16,5 @@ class MITOpenIdConnect(OpenIdConnectAuth):
     REDIRECT_STATE = False
     ACCESS_TOKEN_METHOD = 'POST'
     ID_KEY = 'sub'
-
+    USERNAME_KEY = 'preferred_username'
     DEFAULT_SCOPE = ['openid', 'email', 'profile']
-
-    @_cache(ttl=86400)
-    def oidc_config(self):
-        print("Fetching oidc config...")
-        return self.get_json(self.OIDC_URL +
-                             '/.well-known/openid-configuration')
-
-    ID_TOKEN_ISSUER = property(_autoconf('issuer'))
-    ACCESS_TOKEN_URL = property(_autoconf('token_endpoint'))
-    AUTHORIZATION_URL = property(_autoconf('authorization_endpoint'))
-    REVOKE_TOKEN_URL = property(_autoconf('revocation_endpoint'))
-    USERINFO_URL = property(_autoconf('userinfo_endpoint'))
-    JWKS_URI = property(_autoconf('jwks_uri'))
-
-    @_cache(ttl=86400)
-    def get_jwks_keys(self):
-        keys = KEYS()
-        keys.load_from_url(self.JWKS_URI)
-
-        # Add client secret as oct key so it can be used for HMAC signatures
-        _client_id, client_secret = self.get_key_and_secret()
-        keys.add({'key': client_secret, 'kty': 'oct'})
-        return keys
-
-    def get_and_store_nonce(self, url, state):
-        # Create a nonce
-        nonce = self.strategy.random_string(64)
-        print("Created nonce {0} for url {1}".format(nonce, url))
-        # Store the nonce
-        association = OpenIdConnectAssociation(nonce, assoc_type=state)
-        self.strategy.storage.association.store(url, association)
-        return nonce
-
-    def get_nonce(self, nonce):
-        try:
-            # PSA 0.2.13 checks the nonce against the ACCESS_TOKEN_URL, but
-            # that's the wrong nonce to check.
-            return self.strategy.storage.association.get(
-                server_url=self.AUTHORIZATION_URL,
-                handle=nonce
-            )[0]
-        except IndexError:
-            pass
-
-    def user_data(self, access_token, *args, **kwargs):
-        response = self.request(self.USERINFO_URL,
-                                headers={'Authorization':
-                                         'Bearer {0}'.format(access_token)})
-        data = response.content
-        client_id, _client_secret = self.get_key_and_secret()
-        decryption_key = self.setting('ID_TOKEN_DECRYPTION_KEY')
-        try:
-            user_info = jwt_decode(data, decryption_key, audience=client_id,
-                                   issuer=self.ID_TOKEN_ISSUER,
-                                   leeway=3000,
-                                   algorithms=['HS256'])
-        except InvalidTokenError as err:
-            raise AuthTokenError(self, err)
-
-        return user_info
-
-    def get_user_details(self, response):
-        return {
-            'username': response.get('preferred_username'),
-            'email': response.get('email'),
-            'fullname': response.get('name'),
-            'first_name': response.get('given_name'),
-            'last_name': response.get('family_name'),
-        }
-
-    def validate_and_return_id_token(self, jws):
-        """
-        Validates the id_token according to the spec.
-
-        http://openid.net/specs/openid-connect-core-1_0.html#IDTokenValidation.
-        """
-        try:
-            # Decode the JWT and raise an error if the sig is invalid
-            id_token = JWS().verify_compact(jws.encode('utf-8'),
-                                            self.get_jwks_keys())
-        except JWKESTException:
-            raise AuthTokenError(self,
-                                 'Token error: Signature verification failed')
-        # self.validate_claims(id_token)
-        return id_token
-
-    # def validate_and_return_id_token(self, id_token):
-    #     """
-    #     Validates the id_token according to the steps at
-    #     http://openid.net/specs/openid-connect-core-1_0.html#IDTokenValidation.
-    #     """
-    #     client_id, _client_secret = self.get_key_and_secret()
-    #     decryption_key = self.setting('ID_TOKEN_DECRYPTION_KEY')
-    #     try:
-    #         # Decode the JWT and raise an error if the secret is invalid or
-    #         # the response has expired.
-    #         print(id_token)
-    #         id_token = jwt_decode(id_token, decryption_key, audience=client_id,
-    #                               issuer=self.ID_TOKEN_ISSUER,
-    #                               leeway=3000,
-    #                               algorithms=['HS256'])
-    #     except InvalidTokenError as err:
-    #         raise AuthTokenError(self, err)
-
-    #     # Validate the nonce to ensure the request was not modified
-    #     nonce = id_token.get('nonce')
-    #     if not nonce:
-    #         raise AuthTokenError(self, 'Incorrect id_token: nonce')
-
-    #     print("nonce: {0}".format(nonce))
-
-    #     nonce_obj = self.get_nonce(nonce)
-    #     print("nonce_obj: {0}".format(nonce_obj))
-    #     if nonce_obj:
-    #         self.remove_nonce(nonce_obj.id)
-    #     else:
-    #         raise AuthTokenError(self, 'Incorrect id_token: nonce')
-    #     return id_token
---
+++ src/account/backends/oidc.py
@@ -0,0 +1,282 @@
+from social.backends.open_id import OpenIdConnectAuth
+from social.exceptions import AuthTokenError
+
+import datetime
+import six
+import time
+from calendar import timegm
+
+from jwkest import JWKESTException
+from jwkest.jwk import KEYS
+from jwkest.jws import JWS
+
+
+class _cache(object):
+
+    """
+    Caches method return values for a specific length of time.
+
+    Maintains one cache per class, so subclasses have a different cache entry
+    for the same cached method.
+
+    Does not work for methods with arguments.
+    """
+
+    def __init__(self, ttl):
+        self.ttl = ttl
+        self.cache = {}
+
+    def __call__(self, fn):
+        def wrapped(this):
+            now = time.time()
+            last_updated = None
+            cached_value = None
+            if this.__class__ in self.cache:
+                last_updated, cached_value = self.cache[this.__class__]
+            if not cached_value or now - last_updated > self.ttl:
+                try:
+                    cached_value = fn(this)
+                    self.cache[this.__class__] = (now, cached_value)
+                except:
+                    # Use previously cached value when call fails, if available
+                    if not cached_value:
+                        raise
+            return cached_value
+        return wrapped
+
+
+def _autoconf(name):
+    """fget helper function to fetch values from the OIDC config."""
+    def getter(self):
+        return self.oidc_config().get(name)
+    return getter
+
+
+class DataHubFixedOpenIdConnect(OpenIdConnectAuth):
+
+    """
+    Broader OpenID Connect support than what comes with PSA out of the box.
+
+    This is based on from https://github.com/omab/python-social-auth/pull/747,
+    an unmerged pull request on Python Social Auth.
+
+    PSA 0.2.13's OIDC support is very specific to Google's OIDC server. It
+    only supports HS256 signed tokens, doesn't handle signed userinfo
+    responses, and doesn't handle situations where the OIDC server's clock is
+    ahead of the client's. The pull request is also very specific and has some
+    security vulnerabilities that this fixes.
+
+    To use this, override these constants in a subclass:
+
+        OIDC_URL
+        DEFAULT_SCOPE
+        ID_KEY
+        USERNAME_KEY
+        CLOCK_LEEWAY
+
+    And set these constants in your project's settings based on your OIDC
+    client registration:
+
+        SOCIAL_AUTH_{backend_name}_KEY = OAuth client id
+        SOCIAL_AUTH_{backend_name}_SECRET = OAuth client secret
+        (SOCIAL_AUTH_{backend_name}_ID_TOKEN_DECRYPTION_KEY =
+            SOCIAL_AUTH_{backend_name}_SECRET)
+        SOCIAL_AUTH_{backend_name}_TOKEN_SIGNING_ALGORITHM = ''
+        SOCIAL_AUTH_{backend_name}_USERINFO_SIGNING_ALGORITHM = ''
+    """
+
+    OIDC_URL = None
+    RESPONSE_TYPE = 'code'
+    REDIRECT_STATE = False
+    ACCESS_TOKEN_METHOD = 'POST'
+    ID_KEY = 'sub'
+    USERNAME_KEY = 'preferred_username'
+    DEFAULT_SCOPE = ['openid', 'email', 'profile']
+    # Amount of acceptable clock skew between IdP and this server in seconds.
+    # 5 minutes
+    CLOCK_LEEWAY = 300
+
+    @_cache(ttl=86400)
+    def oidc_config(self):
+        """
+        Discovers the IdP's OIDC configuration.
+
+        Response is cached for 24 hours.
+        """
+        return self.get_json(self.OIDC_URL +
+                             '/.well-known/openid-configuration')
+
+    ID_TOKEN_ISSUER = property(_autoconf('issuer'))
+    ACCESS_TOKEN_URL = property(_autoconf('token_endpoint'))
+    AUTHORIZATION_URL = property(_autoconf('authorization_endpoint'))
+    REVOKE_TOKEN_URL = property(_autoconf('revocation_endpoint'))
+    USERINFO_URL = property(_autoconf('userinfo_endpoint'))
+    JWKS_URI = property(_autoconf('jwks_uri'))
+
+    @_cache(ttl=86400)
+    def get_jwks_keys(self):
+        """
+        Returns the keys used by the IdP.
+
+        Merges client secret into JWK set from server
+        Response is cached for 24 hours.
+        """
+        keys = KEYS()
+        keys.load_from_url(self.JWKS_URI)
+
+        # Add client secret as oct key so it can be used for HMAC signatures
+        _client_id, client_secret = self.get_key_and_secret()
+        keys.add({'key': client_secret, 'kty': 'oct'})
+        return keys
+
+    def get_nonce(self, nonce):
+        """
+        Returns the PSA Association object matching a nonce or None.
+
+        Overridden from PSA default for OpenIdConnectAuth because there's a
+        bug in 0.2.13 where it checked the nonce used for the ACCESS_TOKEN_URL
+        instead of the AUTHORIZATION_URL.
+
+        Used when verifying a request isn't a replay attack. A better name
+        would be get_association_for_nonce(), but this is what PSA uses.
+        """
+        try:
+            # PSA 0.2.13 checks the nonce against the ACCESS_TOKEN_URL, but
+            # that's the wrong nonce to check.
+            return self.strategy.storage.association.get(
+                server_url=self.AUTHORIZATION_URL,
+                handle=nonce
+            )[0]
+        except IndexError:
+            pass
+
+    def validate_userinfo_claims(self, jwt):
+        if jwt['iss'] != self.ID_TOKEN_ISSUER:
+            raise AuthTokenError(self, 'Invalid issuer')
+
+        if 'sub' not in jwt:
+            raise AuthTokenError(self, 'Missing subject')
+
+        client_id, _client_secret = self.get_key_and_secret()
+        if isinstance(jwt['aud'], six.string_types):
+            jwt['aud'] = [jwt['aud']]
+        if client_id not in jwt['aud']:
+            raise AuthTokenError(self, 'Invalid audience')
+
+        # Verify this jwt was issued in the last x minutes.
+        utc_timestamp = timegm(datetime.datetime.utcnow().utctimetuple())
+        if abs(utc_timestamp - jwt['iat']) > self.CLOCK_LEEWAY:
+            raise AuthTokenError(self, 'Incorrect userinfo jwt: iat')
+
+    def validate_id_token_claims(self, id_token):
+        if id_token['iss'] != self.ID_TOKEN_ISSUER:
+            raise AuthTokenError(self, 'Invalid issuer')
+
+        if 'sub' not in id_token:
+            raise AuthTokenError(self, 'Missing subject')
+
+        client_id, _client_secret = self.get_key_and_secret()
+        if isinstance(id_token['aud'], six.string_types):
+            id_token['aud'] = [id_token['aud']]
+        if client_id not in id_token['aud']:
+            raise AuthTokenError(self, 'Invalid audience')
+
+        # Pretty sure this is wrong. Text of spec says "This Claim is only
+        # needed when the ID Token has a single audience value and that
+        # audience is different than the authorized party."
+        if len(id_token['aud']) > 1 and 'azp' not in id_token:
+            raise AuthTokenError(self, 'Incorrect id_token: azp')
+
+        if 'azp' in id_token and id_token['azp'] != client_id:
+            raise AuthTokenError(self, 'Incorrect id_token: azp')
+
+        utc_timestamp = timegm(datetime.datetime.utcnow().utctimetuple())
+        # Verify the token has no expired yet, with x minutes leeway.
+        if utc_timestamp > id_token['exp'] + self.CLOCK_LEEWAY:
+            raise AuthTokenError(self, 'Signature has expired')
+
+        if ('nbf' in id_token and
+                utc_timestamp < id_token['nbf'] - self.CLOCK_LEEWAY):
+            raise AuthTokenError(self, 'Incorrect id_token: nbf')
+
+        # Verify the token was issued in the last x minutes.
+        if abs(utc_timestamp - id_token['iat']) > self.CLOCK_LEEWAY:
+            raise AuthTokenError(self, 'Incorrect id_token: iat')
+
+        # Validate the nonce to ensure the request was not modified
+        nonce = id_token.get('nonce')
+        if not nonce:
+            raise AuthTokenError(self, 'Incorrect id_token: nonce')
+
+        nonce_obj = self.get_nonce(nonce)
+        if nonce_obj:
+            self.remove_nonce(nonce_obj.id)
+        else:
+            raise AuthTokenError(self, 'Incorrect id_token: nonce')
+
+    def user_data(self, access_token, *args, **kwargs):
+        """
+        Returns a user's details from the userinfo endpoint.
+
+        Called by PSA when building info about a user.
+        """
+        # TODO: Extend https://github.com/omab/python-social-auth/pull/747
+        # to include a better version of this -- one which treats the response
+        # as json if the Content-Type is application/json and treats it as a
+        # JWS needing to be verified as below if it's application/jwt.
+        #
+        # To support all servers, that should also support JWEs.
+        #
+        # When PSA is eventually improves its OIDC support, this file can be
+        # simplified.
+        response = self.request(self.USERINFO_URL,
+                                headers={'Authorization':
+                                         'Bearer {0}'.format(access_token)})
+        jws = response.content
+        try:
+            # Decode the JWT and raise an error if the sig is invalid.
+            # Make sure to specify the expected algorithm to prevent attacks
+            # that used an RSA public key as an HMAC secret key.
+            user_info = JWS().verify_compact(
+                jws.encode('utf-8'),
+                keys=self.get_jwks_keys(),
+                sigalg=self.setting('USERINFO_SIGNING_ALGORITHM'))
+        except JWKESTException as e:
+            raise AuthTokenError(
+                self, ('User info error: Signature verification failed. '
+                       'Reason: {0}'.format(e)))
+        print(user_info)
+        self.validate_userinfo_claims(user_info)
+        return user_info
+
+    def get_user_details(self, response):
+        username_key = self.setting('USERNAME_KEY', default=self.USERNAME_KEY)
+        return {
+            'username': response.get(username_key),
+            'email': response.get('email'),
+            'fullname': response.get('name'),
+            'first_name': response.get('given_name'),
+            'last_name': response.get('family_name'),
+        }
+
+    def validate_and_return_id_token(self, jws):
+        """
+        Validates the id_token according to the spec.
+
+        http://openid.net/specs/openid-connect-core-1_0.html#IDTokenValidation.
+        """
+        try:
+            # Decode the JWT and raise an error if the sig is invalid.
+            # Make sure to specify the expected algorithm to prevent attacks
+            # that used an RSA public key as an HMAC secret key.
+            id_token = JWS().verify_compact(
+                jws.encode('utf-8'),
+                keys=self.get_jwks_keys(),
+                sigalg=self.setting('TOKEN_SIGNING_ALGORITHM'))
+        except JWKESTException as e:
+            raise AuthTokenError(
+                self, ('User info error: Signature verification failed. '
+                       'Reason: {0}'.format(e)))
+        print("ID token")
+        self.validate_id_token_claims(id_token)
+        return id_token
