5490939f5b94b15c154e027abcd295f14ac17a45
Justin Anderson <jander@mit.edu>
2015-12-30 17:13:29 -0500

    Make sure initial migration works for new installs.
    Bootstrapping the Site model entirely in settings isn't great.

--- src/config/site_utils.py
+++ src/config/site_utils.py
@@ -1,9 +1,13 @@
 from django.contrib.sites.models import Site
+from django.db.utils import ProgrammingError
 
 
 def set_site_info(domain='datahub-local.mit.edu', name='MIT DataHub'):
-    site = Site.objects.get_current()
-    if site.domain != domain:
-        site.domain = domain
-        site.name = name
-        site.save()
+    try:
+        site = Site.objects.get_current()
+        if site.domain != domain:
+            site.domain = domain
+            site.name = name
+            site.save()
+    except ProgrammingError:
+        pass
