5dfdd5a1080a6fa10a04e33112d79c9ced4b9845
Albert Carter <albert.r.carter@gmail.com>
2016-02-08 16:40:42 -0500

    test more methods on the repo serializer

--- src/api/test/test_repo_serializer.py
+++ src/api/test/test_repo_serializer.py
@@ -39,9 +39,37 @@ class RepoSerializerTests(TestCase):
         mock_manager_create_repo.side_effect = ProgrammingError
 
         success = self.serializer.create_repo('new_repo_name')
-
-        self.assertTrue(mock_manager_create_repo.called)
         self.assertFalse(success)
+
+    def test_delete_repo_happy_path(self):
+        mock_manager_delete_repo = self.mock_manager.return_value.delete_repo
+
+        success = self.serializer.delete_repo('repo_to_be_deleted')
+        self.assertTrue(mock_manager_delete_repo.called)
+        self.assertTrue(success)
+
+    def test_delete_repo_sad_path(self):
+        mock_manager_delete_repo = self.mock_manager.return_value.delete_repo
+        mock_manager_delete_repo.side_effect = ProgrammingError
+
+        success = self.serializer.delete_repo('repo_to_be_deleted')
+        self.assertFalse(success)
+
+    def test_rename_repo_happy_path(self):
+        mock_manager_rename_repo = self.mock_manager.return_value.rename_repo
+
+        success = self.serializer.rename_repo('old_repo_name', 'new_repo_name')
+
+        self.assertTrue(mock_manager_rename_repo.called)
+        self.assertTrue(success)
+
+    def test_rename_repo_sad_path(self):
+        mock_manager_rename_repo = self.mock_manager.return_value.rename_repo
+        mock_manager_rename_repo.side_effect = ProgrammingError
+
+        success = self.serializer.rename_repo('old_repo_name', 'new_repo_name')
+        self.assertFalse(success)
+
 
 # self.manager = 139930597827152
 # self.mock_manager = 139930608555664
