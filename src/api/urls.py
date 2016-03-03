b659580b07947e13b25e7b7ee086476f2a51ff76
Albert Carter <albert.r.carter@gmail.com>
2016-03-03 18:01:56 -0500

    give url params names. swagger GET requests correct. POST query correct.

--- src/api/urls.py
+++ src/api/urls.py
@@ -12,48 +12,45 @@ urlpatterns = [
     # repos
     url(r'^v1/repos/?$', views.Repos.as_view(),
         name='repos_all'),
-    url(r'^v1/repos/(\w+)/?$', views.ReposForUser.as_view(),
+    url(r'^v1/repos/(?P<repo_base>\w+)/?$', views.ReposForUser.as_view(),
         name='repos_specific'),
-    url(r'^v1/repos/(\w+)/(\w+)/?$', views.Repo.as_view(),
+    url(r'^v1/repos/(?P<repo_base>\w+)/(?P<repo_name>\w+)/?$', views.Repo.as_view(),
         name='repo'),
 
-
-
-
     # collaborators
-    url(r'^v1/repos/(\w+)/(\w+)/collaborators/(\w+)/?$',
+    url(r'^v1/repos/(?P<repo_base>\w+)/(?P<repo_name>\w+)/collaborators/(?P<collaborator>\w+)/?$',
         views.Collaborator.as_view(), name='collaborator'),
-    url(r'^v1/repos/(\w+)/(\w+)/collaborators/?$',
+    url(r'^v1/repos/(?P<repo_base>\w+)/(?P<repo_name>\w+)/collaborators/?$',
         views.Collaborators.as_view(), name='collaborators'),
 
     # tables
-    url(r'^v1/repos/(\w+)/(\w+)/tables/(\w+)/?$',
+    url(r'^v1/repos/(?P<repo_base>\w+)/(?P<repo_name>\w+)/tables/(?P<table>\w+)/?$',
         views.Table.as_view(), name='table'),
-    url(r'^v1/repos/(\w+)/(\w+)/tables/?$',
+    url(r'^v1/repos/(?P<repo_base>\w+)/(?P<repo_name>\w+)/tables/?$',
         views.Tables.as_view(), name='tables'),
 
     # views
-    url(r'^v1/repos/(\w+)/(\w+)/views/(\w+)/?$',
+    url(r'^v1/repos/(?P<repo_base>\w+)/(?P<repo_name>\w+)/views/(?P<view>\w+)/?$',
         views.View.as_view(), name='view'),
-    url(r'^v1/repos/(\w+)/(\w+)/views/?$',
+    url(r'^v1/repos/(?P<repo_base>\w+)/(?P<repo_name>\w+)/views/?$',
         views.Views.as_view(), name='views'),
 
     # cards
-    url(r'^v1/repos/(\w+)/(\w+)/cards/(\w+)/?$',
+    url(r'^v1/repos/(?P<repo_base>\w+)/(?P<repo_name>\w+)/cards/(?P<card_name>\w+)/?$',
         views.Card.as_view(), name='card'),
-    url(r'^v1/repos/(\w+)/(\w+)/cards/?$',
+    url(r'^v1/repos/(?P<repo_base>\w+)/(?P<repo_name>\w+)cards/?$',
         views.Cards.as_view(), name='cards'),
 
     # files
-    url(r'^v1/repos/(\w+)/(\w+)/files/([\w\s\.]+)$',
+    url(r'^v1/repos/(?P<repo_base>\w+)/(?P<repo_name>\w+)/files/(?P<file_name>[\w\s\.]+)$',
         views.File.as_view(), name='file'),
-    url(r'^v1/repos/(\w+)/(\w+)/files/?$',
+    url(r'^v1/repos/(?P<repo_base>\w+)/(?P<repo_name>\w+)/files/?$',
         views.Files.as_view(), name='files'),
 
     # query
-    url(r'^v1/query/(\w+)/(\w+)/?$', views.Query.as_view(),
-        name='query_with_repo'),
-    url(r'^v1/query/(\w+)/?$', views.Query.as_view(),
+    url(r'^v1/query/(?P<repo_base>\w+)/(?P<repo_name>\w+)/?$',
+        views.Query.as_view(), name='query_with_repo'),
+    url(r'^v1/query/(?P<repo_base>\w+)/?$', views.Query.as_view(),
         name='query'),
 
 ]
