7da52a20be52d414b2c032c5e62d7624d25fb95f
Gina Yuan <gina.yuan@gmail.com>
2016-03-04 11:37:52 -0500

    console oauth

--- src/apps/console/views.py
+++ src/apps/console/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 
-
 '''
 @author: Anant Bhardwaj
 @date: Mar 21, 2013
@@ -12,5 +11,11 @@ Datahub Console
 
 @login_required
 def index(request):
-    return render_to_response("console.html", {
+    res = {'login': request.user.get_username()}
+    return render_to_response("console.html", res)
+
+
+@login_required
+def index2(request):
+    return render_to_response("console2.html", {
         'login': request.user.get_username()})
