115374a1e4cdf9b0aed03a1fc9946503f7e93e87
Albert Carter <albert.r.carter@gmail.com>
2016-03-09 10:12:24 -0500

    update run-functional-tests script to use test_db

--- src/scripts/run-functional-tests.sh
+++ src/scripts/run-functional-tests.sh
@@ -4,4 +4,4 @@ cd /datahub/src
 set -x
 python manage.py test functional_tests.test_login_auth
 python manage.py test functional_tests.test_layout_and_styling
-python manage.py test functional_tests.test_database_capabilities
+python manage.py test functional_tests.test_db
