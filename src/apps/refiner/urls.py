44a543bc243aafabfc1476c721a80172c921b2ba
Albert Carter <albert.r.carter@gmail.com>
2016-03-14 17:06:05 -0400

    pep8 refiner/distill

--- src/apps/refiner/urls.py
+++ src/apps/refiner/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('',    
-    url(r'^$', 'refiner.views.index'),
-    url(r'^refine-data$', 'refiner.views.refine_data'),
-)
+urlpatterns = patterns('',
+                       url(r'^$', 'refiner.views.index'),
+                       url(r'^refine-data$', 'refiner.views.refine_data'),
+                       )
