ee0ef7c011fefe0b6b0b65ea6ebe60b8b7b1cc4b
Justin Anderson <jander@mit.edu>
2016-03-23 17:13:58 -0400

    Add basic tests for repo and query endpoints.

--- src/api/test/test_endpoints.py
+++ src/api/test/test_endpoints.py
@@ -3,9 +3,9 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 from core.db.manager import DataHubManager
-from core.db.backend.pg import _close_all_connections
 import random
 import string
+from collections import namedtuple
 
 
 def random_slug(length):
@@ -18,7 +18,7 @@ class APIEndpointTests(APITestCase):
     """docstring for APIEndpointTests"""
 
     def setUp(self):
-        self.username = "asldbhjdefhijklmnopqr"
+        self.username = "delete_me_api_test_user"
         self.email = self.username + "@mit.edu"
         self.password = self.username
         self.user = User.objects.create_user(
@@ -27,11 +27,10 @@ class APIEndpointTests(APITestCase):
 
     def tearDown(self):
         # Force close all outstanding db connections to this user's database.
-        _close_all_connections(repo_base=self.username)
         DataHubManager.remove_user(self.username)
 
 
-class UserEndpointTests(APIEndpointTests):
+class CurrentUserTests(APIEndpointTests):
 
     def test_get_user(self):
         url = reverse('api:user')
@@ -44,31 +43,258 @@ class UserEndpointTests(APIEndpointTests):
             })
 
 
-class UserReposEndpointTests(APIEndpointTests):
+class CurrentUserReposTests(APIEndpointTests):
 
     def test_get_user_repos(self):
-        # import pdb; pdb.set_trace()
         url = reverse('api:user_repos')
         response = self.client.get(url, follow=True)
-        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {'repos': []})
 
 
-class ReposEndpointTests(APIEndpointTests):
+class ReposTests(APIEndpointTests):
 
     def test_get_repos(self):
-        # url = reverse('api:repos_all')
-        # response = self.client.get(url, follow=True)
-        # print(response.data)
-        # self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # self.assertEqual(response.data, {'repos': []})
-        pass
+        url = reverse('api:repos_all')
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'repos': []})
 
-    # def test_create_repo(self):
-    #     url = reverse('api:repos_all')
-    #     data = {'repo_name': 'repo_one'}
-    #     response = self.client.post(url, data, follow=True, format='json')
-    #     print(response.data)
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertEqual(response.data, {'repos': []})
+
+class RepoTests(APIEndpointTests):
+
+    def test_get_repo(self):
+        # Make sure it's a 404 when there are no repos.
+        repo_name = 'repo_one'
+        url = reverse('api:repo',
+                      kwargs={'repo_base': self.username,
+                              'repo_name': repo_name})
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data,
+                         {'error_type': 'LookupError',
+                          'detail': 'Invalid repository name: repo_one'})
+
+        # Create a repo and make sure it's a 200.
+        with DataHubManager(self.username) as manager:
+            manager.create_repo(repo_name)
+
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data,
+                         {'files': [],
+                          'tables': [],
+                          'collaborators': [],
+                          'views': [],
+                          'cards': [],
+                          'owner': {'username': u'delete_me_api_test_user'}})
+
+    def test_patch_repo(self):
+        repo_name = 'repo_one'
+        url = reverse('api:repo',
+                      kwargs={'repo_base': self.username,
+                              'repo_name': repo_name})
+
+        # Try renaming a repo that doesn't exist
+        response = self.client.patch(
+            url, {'new_name': repo_name}, follow=True, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data,
+                         {'pgcode': '3F000',
+                          'error_type': 'ProgrammingError',
+                          'detail': 'schema "' + repo_name +
+                          '" does not exist\n',
+                          'severity': 'ERROR'})
+
+        with DataHubManager(self.username) as manager:
+            manager.create_repo(repo_name)
+
+        # Try renaming a repo to its current name
+        response = self.client.patch(
+            url, {'new_name': 'repo_one'}, follow=True, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data,
+                         {'pgcode': '42P06',
+                          'error_type': 'ProgrammingError',
+                          'detail': 'schema "' + repo_name +
+                          '" already exists\n',
+                          'severity': 'ERROR'})
+
+        # Try renaming for real
+        response = self.client.patch(
+            url, {'new_name': 'repo_five_thousand'},
+            follow=True, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data,
+                         {'files': [],
+                          'tables': [],
+                          'collaborators': [],
+                          'views': [],
+                          'cards': [],
+                          'owner': {'username': u'delete_me_api_test_user'}})
+
+    def test_delete_repo(self):
+        # Make sure it's a 404 when there are no repos.
+        repo_name = 'repo_one'
+        url = reverse('api:repo',
+                      kwargs={'repo_base': self.username,
+                              'repo_name': repo_name})
+        response = self.client.delete(url, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data,
+                         {'pgcode': '3F000',
+                          'error_type': 'ProgrammingError',
+                          'detail': 'schema "repo_one" does not exist\n',
+                          'severity': 'ERROR'})
+
+        # Create a repo and make sure it's a 200.
+        with DataHubManager(self.username) as manager:
+            manager.create_repo(repo_name)
+
+        response = self.client.delete(url, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(response.data, None)
+
+
+class ReposForUserTests(APIEndpointTests):
+
+    def _expected_response(self, owner, repo_names):
+        result = []
+        base = 'http://testserver/api/v1/repos/'
+        for repo in repo_names:
+            result.append({'owner': owner,
+                           'href': '{0}{1}/{2}'.format(base, owner, repo),
+                           'repo_name': repo})
+        return {'repos': result}
+
+    def test_get_repos(self):
+        # Expect an empty array when the user has no repos
+        url = reverse('api:repos_specific',
+                      kwargs={'repo_base': self.username})
+
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, self._expected_response(
+            self.username, []))
+
+        # Add some repos and expect they show up
+        repos = ['foo', 'bar', 'baz']
+        with DataHubManager(self.username) as manager:
+            [manager.create_repo(repo) for repo in repos]
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Sort because the results will be sorted
+        self.assertEqual(response.data, self._expected_response(
+            self.username, sorted(repos)))
+
+    def test_create_repo(self):
+        # Create two repos
+        url = reverse('api:repos_specific',
+                      kwargs={'repo_base': self.username})
+
+        response = self.client.post(
+            url, {'repo_name': 'repo_one'}, follow=True, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, self._expected_response(
+            self.username, ['repo_one']))
+        response = self.client.post(
+            url, {'repo_name': 'repo_two'}, follow=True, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, self._expected_response(
+            self.username, ['repo_one', 'repo_two']))
+
+
+Query = namedtuple('Query', ['sql', 'status_code', 'expect'])
+
+
+class QueryTests(APIEndpointTests):
+
+    def test_query_with_repo(self):
+        repo_name = 'repo_one'
+        table_name = 'sandwiches'
+        repo_table = repo_name + '.' + table_name
+        with DataHubManager(self.username) as manager:
+            manager.create_repo(repo_name)
+
+        url = reverse('api:query_with_repo',
+                      kwargs={'repo_base': self.username,
+                              'repo_name': repo_name})
+        queries = [
+            Query(sql="""
+                  CREATE TABLE """ + table_name + """ (
+                      name varchar (255) NOT NULL,
+                      deliciousness numeric,
+                      is_deep_fried boolean);
+                  """,
+                  status_code=status.HTTP_200_OK,
+                  expect={}),
+            Query(sql="SELECT * FROM " + repo_table + ";",
+                  status_code=status.HTTP_200_OK,
+                  expect=[]),
+            Query(sql="INSERT INTO " + repo_table +
+                      " VALUES ('reuben', 25, FALSE);",
+                  status_code=status.HTTP_200_OK,
+                  expect={}),
+            Query(sql="SELECT * FROM " + repo_table + ";",
+                  status_code=status.HTTP_200_OK,
+                  expect=[
+                      {'is_deep_fried': False,
+                       'deliciousness': 25,
+                       'name': 'reuben'}]),
+            Query(sql="DROP TABLE " + repo_table,
+                  status_code=status.HTTP_200_OK,
+                  expect={}),
+        ]
+
+        for q in queries:
+            response = self.client.post(
+                url, {'query': q.sql}, follow=True, format='json')
+            print(response.data)
+            self.assertEqual(response.status_code, q.status_code)
+            self.assertEqual(response.data, q.expect)
+
+    def test_post_query(self):
+        repo_name = 'repo_one'
+        table_name = 'sandwiches'
+        with DataHubManager(self.username) as manager:
+            manager.create_repo(repo_name)
+        url = reverse('api:query',
+                      kwargs={'repo_base': self.username})
+
+        queries = [
+            Query(sql="""
+                  CREATE TABLE """ + table_name + """ (
+                      name varchar (255) NOT NULL,
+                      deliciousness numeric,
+                      is_deep_fried boolean);
+                  """,
+                  status_code=status.HTTP_200_OK,
+                  expect={}),
+            Query(sql="SELECT * FROM " + table_name + ";",
+                  status_code=status.HTTP_200_OK,
+                  expect=[]),
+            Query(sql="INSERT INTO " + table_name +
+                      " VALUES ('reuben', 25, FALSE);",
+                  status_code=status.HTTP_200_OK,
+                  expect={}),
+            Query(sql="SELECT * FROM " + table_name + ";",
+                  status_code=status.HTTP_200_OK,
+                  expect=[
+                      {'is_deep_fried': False,
+                       'deliciousness': 25,
+                       'name': 'reuben'}]),
+            Query(sql="DROP TABLE " + table_name,
+                  status_code=status.HTTP_200_OK,
+                  expect={}),
+        ]
+
+        for q in queries:
+            response = self.client.post(
+                url, {'query': q.sql}, follow=True, format='json')
+            print(response.data)
+            self.assertEqual(response.status_code, q.status_code)
+            self.assertEqual(response.data, q.expect)
+
+    # Test that Accept:text/csv works
+    # Test that pagination works
+    # Test that responses give metadata
