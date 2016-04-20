c081eb9f1da41294c3c1b9c08271c24198b96882
Gina Yuan <gina.yuan@gmail.com>
2016-04-20 14:18:50 -0400

    Updated dataq build files with most recent directories

--- src/apps/dataq/gulpfile.js
+++ src/apps/dataq/gulpfile.js
@@ -1,5 +1,5 @@
-var del = require("del");
-var gulp = require("gulp");
+var del = require('del');
+var gulp = require('gulp');
 var watch = require('gulp-watch');
 var handlebars = require('gulp-handlebars');
 var wrap = require('gulp-wrap');
@@ -10,52 +10,66 @@ var path = require('path');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 
-var static_base = "../../browser/static/dataq/";
+var static_base = '../../browser/static/dataq/';
 
-gulp.task("default", ["clean", "templates", "styles", "scripts", "index", "watch"]);
+// 'gulp' rebuilds all files
+gulp.task('default', ['templates', 'styles', 'scripts', 'index', 'watch']);
 
-gulp.task('templates', ["clean"], function() {
-  gulp.src("./client_src/templates/*.hbs")
+gulp.task('templates', function() {
+  gulp.src('./client_src/templates/*.hbs')
     .pipe(handlebars())
-    .on("error", function(err) { console.log(err.message) })
-    .pipe(wrap("Handlebars.template(<%= contents %>)"))
+    .on('error', function(err) { console.log(err.message) })
+    .pipe(wrap('Handlebars.template(<%= contents %>)'))
     .pipe(declare({
-      namespace: "DataQ.templates",
+      namespace: 'DataQ.templates',
       noRedeclare: true
     }))
     .pipe(concat('templates.js'))
     .pipe(gulp.dest(static_base));
 });
 
-gulp.task('styles', ["clean"], function() {
-  gulp.src("./client_src/less/*.less")
+gulp.task('styles', function() {
+  gulp.src('./client_src/less/*.less')
     .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
+      paths: [path.join(__dirname, 'less', 'includes')]
     }))
     .pipe(gulp.dest(static_base + 'css/'));
 });
 
-gulp.task("clean", function(cb) {
-  del([
-    static_base + "**"
-  ], {force: true}, cb);
-});
-
-gulp.task("scripts", ["clean"], function() {
-  gulp.src("./client_src/js/*.js")
+gulp.task('scripts', function() {
+  gulp.src('./client_src/js/*.js')
     .pipe(sourcemaps.init())
-    .pipe(concat("dataq.min.js"))
+    .pipe(concat('dataq.min.js'))
     .pipe(uglify())
-    .on("error", function(err){console.log(err);})
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest(static_base + "js/"))
+    .on('error', function(err) {console.log(err);})
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(static_base + 'js/'));
 });
 
-gulp.task("index", ["clean"], function() {
-  gulp.src("./client_src/index.html")
+gulp.task('index', function() {
+  gulp.src('./client_src/index.html')
     .pipe(gulp.dest(static_base));
 });
 
-gulp.task("watch", ["clean"], function() {
-  gulp.watch("./client_src/**/*.*", ["clean", "templates", "styles", "scripts", "index"]);
+// 'gulp clean' removes all files
+gulp.task('clean', function() {
+  del([
+    static_base + '**'
+  ], {force: true});
+});
+
+// 'gulp watch' builds files automatically as they are modifed
+gulp.task('watch', function() {
+  gulp.watch('./client_src/templates/*.hbs', function() {
+    gulp.run('templates');
+  });
+  gulp.watch('./client_src/less/*.less', function() {
+    gulp.run('styles');
+  });
+  gulp.watch('./client_src/js/*.js', function() {
+    gulp.run('scripts');
+  });
+  gulp.watch('./client_src/index.html', function() {
+    gulp.run('index');
+  });
 });
